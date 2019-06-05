3c5e79130af836e348f8455a21602f2bf81c14c2
Philipp <philipp.holl@tum.de>
2019-06-05 13:20:42 +0200

    Fix simulation101.py, tumsmoke.py, remove unified_simulation.py

--- apps/simulation101.py
+++ apps/simulation101.py
@@ -2,18 +2,17 @@ from phi.flow import *
 from phi.model import FieldSequenceModel
 
 
-smoke = Smoke(Domain([80, 64], SLIPPERY))
+smoke = Smoke(Domain([64, 64], SLIPPERY))
+inflow(Sphere((10,32), 5), rate=0.2)
 
 
 class Simulation101(FieldSequenceModel):
 
     def __init__(self):
-        FieldSequenceModel.__init__(self, "PhiFlow - Simulation 101", "Smoke simulation with NumPy", stride=10)
+        FieldSequenceModel.__init__(self, "PhiFlow - Simulation 101", "Smoke simulation with NumPy", stride=5)
         self.state = smoke.empty()
-        inflow(box[8, 12:64-12])
         self.add_field("Density", lambda: self.state.density)
         self.add_field("Velocity", lambda: self.state.velocity)
-        self.step()
 
     def step(self):
         self.state = smoke.step(self.state)
--- apps/tumsmoke.py
+++ apps/tumsmoke.py
@@ -1,31 +1,38 @@
 from phi.tf.model import *
+from phi.flow import *
 import sys
 
 
-def build_inflow(sim):
-    size = sim.dimensions
-    le_x = int(math.floor(size[1] * 0.7 / 10))
-    le_y = int(math.floor(size[0] * 0.7 / 10))
-    offset_x = int(0.5 * math.floor(size[1] - le_x * 10))
-    offset_y = int(size[0] - math.floor(size[0] * 0.1) - (le_y * 7))
-    inflow_density = sim.zeros()
-    inflow_density[:, size[0] // 16, 2*le_x:3*le_x] = 1
-    inflow_density[:, size[0] // 16, size[1]-3*le_x:size[1]-2*le_x] = 1
-    inflow_density[:, offset_y+le_y+1, offset_x+4*le_x:offset_x+4*le_x+le_x//2, :] = 0.1
-    return inflow_density
+size = [128]*2
+if len(sys.argv) > 1:
+    size = [int(sys.argv[1])]*2
+smoke = Smoke(Domain(size, SLIPPERY))
 
 
-def create_tum_logo(sim):
-    size = sim.dimensions
-    le_x = int(math.floor(size[1] * 0.7 / 10))
-    le_y = int(math.floor(size[0] * 0.7 / 10))
-    offset_x = int(0.5 * math.floor(size[1] - le_x * 10))
-    offset_y = int(size[0] - math.floor(size[0] * 0.1) - (le_y * 7))
+def build_inflow():
+    y, x = smoke.dimensions
+    dx, dy = x * 0.07, y * 0.07
+    offset_x = 0.5 * (x - dx * 10)
+    offset_y = y - y * 0.1 - dy * 7
+    inflow(box[y/16, 2*dx:3*dx])
+    inflow(box[y/16, x-3*dx:x-2*dx])
+    inflow(box[offset_y+dy+1, offset_x+4*dx:offset_x+4.5*dx], 0.1)
+
+
+def create_tum_logo():
+    y, x = smoke.dimensions
+    dx, dy = x * 0.07, y * 0.07
+    offset_x = 0.5 * (x - dx * 10)
+    offset_y = y - y * 0.1 - dy * 7
     for i in range(1, 10, 2):
-        sim.set_obstacle((le_y * 6, le_x), (offset_y, offset_x + i * le_x))
-    sim.set_obstacle((le_y, le_x), (offset_y, offset_x + 4 * le_x))
-    sim.set_obstacle((le_y, le_x * 4), (offset_y + 6 * le_y, offset_x))
-    sim.set_obstacle((le_y, le_x * 5), (offset_y + 6 * le_y, offset_x + 5 * le_x))
+        obstacle(box[offset_y:offset_y+6*dy, offset_x + i * dx:offset_x + (i+1) * dx])
+    obstacle(box[offset_y:offset_y+dy, offset_x + 4 * dx:offset_x + 4 * dx+ dx])
+    obstacle(box[offset_y + 6 * dy:offset_y + 7 * dy, offset_x:offset_x+dx * 4])
+    obstacle(box[offset_y + 6 * dy:offset_y + 7 * dy, offset_x + 5 * dx:offset_x + 10 * dx])
+
+
+build_inflow()
+create_tum_logo()
 
 
 class SimpleplumeTF(TFModel):
@@ -33,31 +40,17 @@ class SimpleplumeTF(TFModel):
     def __init__(self, size):
         TFModel.__init__(self, "TUMsmoke", "Smoke simulation with obstacles",
                          summary="TUMsmoke" + "x".join([str(d) for d in size]), stride=20)
-        sim = self.sim = TFFluidSimulation(size, "closed", force_use_masks=True)
-        velocity = self.velocity_in = self.sim.placeholder("staggered", "velocity")
-        density = self.density_in = self.sim.placeholder(1, "density")
-        self.inflow_density = build_inflow(self.sim)
-        self.action_reset()
-        self.density_out = sim.conserve_mass(velocity.advect(density), density) + self.inflow_density
-        self.velocity_out = sim.divergence_free(velocity.advect(velocity) + sim.buoyancy(self.density_out))
-        self.finalize_setup([])
-        create_tum_logo(self.sim)
-
-        self.add_field("Density", lambda: self.density_data)
-        self.add_field("Velocity", lambda: self.velocity_data)
-        self.add_field("Remaining Divergence", lambda: self.velocity_data.divergence())
-        self.add_field("Obstacles", lambda: self.sim.run(self.sim.extended_active_mask))
+        self.state = smoke.empty()
+        self.add_field("Density", lambda: self.state.density)
+        self.add_field("Velocity", lambda: self.state.velocity)
+        self.add_field("Domain", lambda: smoke.domainstate.active(extend=1))
 
     def step(self):
-        self.velocity_data, self.density_data = self.sim.run([self.velocity_out, self.density_out],
-                feed_dict={self.velocity_in.staggered: self.velocity_data, self.density_in: self.density_data})
+        self.state = smoke.step(self.state)
 
     def action_reset(self):
         self.time = 0
-        self.velocity_data = self.sim.zeros("staggered")
-        self.density_data = self.inflow_density
+        self.state = smoke.empty()
 
 
-size = 128
-if len(sys.argv) > 1: size = int(sys.argv[1])
 app = SimpleplumeTF([size]*2).show(display=("Density", "Velocity"), framerate=2, production=__name__ != "__main__")
