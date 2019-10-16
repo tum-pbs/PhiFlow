f4aabfb25d82cf7ac3146b95464a593213fe7d6a
Philipp <philipp.holl@tum.de>
2019-10-16 14:33:11 +0200

    Simplify smoke_logo, remove manual_smoke, fixes

--- demos/moving_inflow.py
+++ demos/moving_inflow.py
@@ -17,11 +17,11 @@ class MovingInflowDemo(FieldSequenceModel):
         smoke = world.Smoke(Domain([64, 64], SLIPPERY))
         inflow = world.Inflow(inflow_at(0), rate=0.2)
         inflow.physics = GeometryMovement(inflow_at)
-        obstacle = Obstacle(obstacle_at(0), SLIPPERY)
+        obstacle = world.Obstacle(obstacle_at(0), SLIPPERY)
         obstacle.physics = GeometryMovement(obstacle_at)
         self.add_field('Density', lambda: smoke.density)
         self.add_field('Velocity', lambda: smoke.velocity)
-        self.add_field('Domain', lambda: smoke.domaincache.active(1))
+        self.add_field('Domain', lambda: obstacle_mask(smoke).at(smoke.density))
 
 
 app = MovingInflowDemo().show(display=('Density', 'Velocity'), production=__name__ != '__main__')
