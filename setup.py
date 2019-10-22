0e89141cdbc3e62188bab3da415a3f28db7f119a
ga67fet <ga67fet@mytum.de>
2019-10-22 22:00:41 +0200

    Add missing packages to setup.py

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ extras = {
 setup(
     name='phiflow',
     version='0.4.0',
-    packages=['phi', 'phi.data', 'phi.local', 'phi.math', 'phi.physics', 'phi.solver', 'phi.tf', 'phi.viz'],
+    packages=['phi', 'phi.data', 'phi.field', 'phi.local', 'phi.math', 'phi.physics', 'phi.solver', 'phi.struct', 'phi.tf', 'phi.viz'],
     cmdclass={
         "cuda": CudaCommand,
     },
