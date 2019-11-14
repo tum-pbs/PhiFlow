e29c2d55b6616c22bbe2004be184584e013370dd
the-rccg <r.greif@outlook.com>
2019-11-14 20:17:31 +0100

    demo linting

--- demos/simple_tfmodel.py
+++ demos/simple_tfmodel.py
@@ -1,24 +1,24 @@
 from phi.tf.flow import *
 
 
-resolution = y, x = 64, 64
-datapath = '~/phi/data/smoke/' # at least 10 sims, has to match resolution
+RESOLUTION = y, x = 64, 64
+DATAPATH = '~/phi/data/smoke/' # at least 10 sims, has to match RESOLUTION
 
 
 def network(density):
     """very simple conv net"""
-    c = 32  # number of features in inner layers
-    W1 = tf.get_variable('W1', [5, 5, 1, c], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
-    f1 = tf.nn.relu(tf.nn.conv2d(density, W1, strides=[1, 1, 1, 1], padding='SAME'))
+    n_feat = 32  # number of features in inner layers
+    w_1 = tf.get_variable('w_1', [5, 5, 1, n_feat], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
+    f_1 = tf.nn.relu(tf.nn.conv2d(density, w_1, strides=[1, 1, 1, 1], padding='SAME'))
 
-    W2 = tf.get_variable('W2', [5, 5, c, c], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
-    f2 = tf.nn.relu(tf.nn.conv2d(f1, W2, strides=[1, 1, 1, 1], padding='SAME'))
+    w_2 = tf.get_variable('w_2', [5, 5, n_feat, n_feat], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
+    f_2 = tf.nn.relu(tf.nn.conv2d(f_1, w_2, strides=[1, 1, 1, 1], padding='SAME'))
 
-    W3 = tf.get_variable('W3', [3, 3, c, c], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
-    f3 = tf.nn.relu(tf.nn.conv2d(f2, W3, strides=[1, 1, 1, 1], padding='SAME'))
+    w_3 = tf.get_variable('w_3', [3, 3, n_feat, n_feat], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
+    f_3 = tf.nn.relu(tf.nn.conv2d(f_2, w_3, strides=[1, 1, 1, 1], padding='SAME'))
 
-    Wo = tf.get_variable('Wo', [3, 3, c, 2], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
-    fo = tf.nn.conv2d(f3, Wo, strides=[1, 1, 1, 1], padding='SAME')  # no activation! we have negative values in the GT fields
+    Wo = tf.get_variable('Wo', [3, 3, n_feat, 2], initializer=tf.random_normal_initializer(stddev=0.01, mean=0.))
+    fo = tf.nn.conv2d(f_3, Wo, strides=[1, 1, 1, 1], padding='SAME')  # no activation! we have negative values in the GT fields
 
     print(format(fo.shape))
     return fo
@@ -30,7 +30,7 @@ class TrainingTest(TFApp):
         TFApp.__init__(self, 'Training',
                        learning_rate=2e-4,
                        validation_batch_size=4, training_batch_size=8)
-        smoke_in, load_dict = load_state(Smoke(Domain(resolution)))
+        smoke_in, load_dict = load_state(Smoke(Domain(RESOLUTION)))
 
 
         with self.model_scope():
@@ -42,8 +42,8 @@ class TrainingTest(TFApp):
 
         # this assumes we have 10 sims in the path
         self.set_data(dict=load_dict,
-                      train=Dataset.load(datapath, range(0, 8)),
-                      val=Dataset.load(datapath, range(8, 10)))
+                      train=Dataset.load(DATAPATH, range(0, 8)),
+                      val=Dataset.load(DATAPATH, range(8, 10)))
 
         self.add_field('Velocity (Ground Truth)', smoke_in.velocity)
         self.add_field('Velocity (Model)', pred_vel)
