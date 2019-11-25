22f569f649115ed5b8cc71d8d1680dd48dc24a4c
Philipp <philipp.holl@tum.de>
2019-11-25 12:32:13 +0100

    Print warning when using TensorFlow 2

--- phi/tf/flow.py
+++ phi/tf/flow.py
@@ -5,3 +5,10 @@ from .app import *
 from .session import *
 from .world import *
 from .data import *
+from .util import *
+import tensorflow as tf
+
+
+if int(tf.__version__[0]) > 1:
+    import warnings
+    warnings.warn('TensorFlow 2 is not fully supported by PhiFlow.')
