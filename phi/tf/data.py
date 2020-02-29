b76cd63245a9a7b30e3918b60e61232c6096fa0e
Philipp <Philipp@MHoll.de>
2020-02-29 16:37:55 +0100

    Add docstrings for App.set_data, tf.data.load_state

--- phi/tf/data.py
+++ phi/tf/data.py
@@ -8,12 +8,19 @@ from .util import placeholder
 
 
 def load_state(state):
+    """
+Create placeholders for tensors in the supplied state.
+    :param state: State or StateProxy
+    :return:
+      1. Valid state containing or derived from created placeholders
+      2. dict mapping from placeholders to their default names (using struct.names)
+    """
     if isinstance(state, StateProxy):
         state = state.state
     assert isinstance(state, State)
     state = _transform_for_writing(state)
     names = struct.names(state)
     with _unsafe():
-        placeholders = placeholder(state.shape)
+        placeholders = placeholder(state.staticshape)
     state_in = struct.map(lambda x: x, placeholders)  # validates fields, splits staggered tensors
     return state_in, {placeholders: names}
