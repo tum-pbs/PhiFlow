6250bc1041e5f535a510965f2c78ed8660cfc78d
Philipp <Philipp@MHoll.de>
2020-03-25 18:51:20 +0100

    Simplify demos
    Simulation scripts now instantiate App without subclassing it

--- demos/burgers_sim.py
+++ demos/burgers_sim.py
@@ -1,11 +1,6 @@
 from phi.flow import *
 
+domain = Domain([64, 64], boundaries=PERIODIC)
+world.add(BurgersVelocity(domain, velocity=lambda s: math.randfreq(s) * 2), physics=Burgers())
 
-class BurgersEquation(App):
-
-    def __init__(self, domain=Domain([64, 64], boundaries=PERIODIC)):
-        App.__init__(self, framerate=5)
-        world.add(BurgersVelocity(domain, velocity=lambda s: math.randfreq(s) * 2), physics=Burgers())
-
-
-show()
+show(App('Burgers Equation in %dD' % len(domain.resolution), framerate=5))
