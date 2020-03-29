458e539c34c00a2a1c2eb7c0fbbab187afdff766
the-rccg <r.greif@outlook.com>
2020-03-29 12:49:24 +0200

    Add reconstruction test

--- tests/test_poisson_solve.py
+++ tests/test_poisson_solve.py
@@ -6,6 +6,7 @@ from phi import math
 from phi.flow import CLOSED, PERIODIC, OPEN, Domain, poisson_solve, Noise
 from phi.physics.pressuresolver.geom import GeometricCG
 from phi.physics.pressuresolver.sparse import SparseCG, SparseSciPy
+from phi.physics.field import CenteredGrid
 
 
 def _generate_examples():
@@ -67,6 +68,7 @@ DOMAINS = [
             Domain([4, 5], boundaries=PERIODIC),
             Domain([4, 5], boundaries=[CLOSED, PERIODIC]),
             Domain([4, 5], boundaries=[CLOSED, OPEN]),
+            Domain([4, 5], boundaries=[PERIODIC, OPEN]),
         ]
 
 
@@ -75,7 +77,8 @@ class TestPoissonSolve(TestCase):
     def test_equal_results(self):
         data_in = _generate_examples()
         for domain in DOMAINS:
-            pressure_fields = [poisson_solve(domain.centered_grid(data_in), domain, solver=solver)[0].data for solver in [SparseCG(), GeometricCG()]]
+            pressure_fields = [poisson_solve(domain.centered_grid(data_in), domain, solver=solver)[0].data
+                               for solver in [SparseCG(), GeometricCG()]]
             for field in pressure_fields[1:]:
                 np.testing.assert_almost_equal(field, pressure_fields[0], decimal=4)
 
@@ -87,3 +90,70 @@ class TestPoissonSolve(TestCase):
 
     def test_geometric_cg(self):
         _test_all(GeometricCG())
+
+
+def run_second_order_fft_reconstruction(in_field, set_accuracy, second_order_tolerance=20):
+    # Second Order FFT test
+    mean = math.mean(in_field).data
+    centered_field = in_field - mean
+    fft_poisson2 = math.fourier_poisson(in_field, times=2)
+    fft_poisson2 += mean
+    fft_reconst2 = fft_poisson2.laplace().laplace()
+    error2 = (in_field - fft_reconst2)/in_field
+    max_error2 = np.max(np.abs(error2.data))
+    assert max_error2 < second_order_tolerance*set_accuracy, "{}^2 reconstruction not within set accuracy. {:.2g} vs. {:.2g}".format('FFT*2', max_error2, second_order_tolerance*set_accuracy)
+
+def test_reconstruction_first_order(in_field, solve_func, set_accuracy, name, first_order_tolerance=2):
+    # Test Reconstruction
+    mean = math.mean(in_field).data
+    centered_field = in_field - mean
+    ret = solve_func(centered_field)
+    try:
+        solved_field, it = ret
+    except:
+        solved_field = ret
+    reconst1 = solved_field.laplace() + mean  # Reconstruct Input
+    error = (in_field - reconst1)/in_field
+    max_error = np.max(np.abs(error.data))
+    assert max_error < first_order_tolerance*set_accuracy, "{} reconstruction not within set accuracy. {:.2g} vs. {:.2g}".format(name, max_error, first_order_tolerance*set_accuracy)
+
+def test_reconstruction_second_order(in_field, solve_func, set_accuracy, name, second_order_tolerance=20):
+    # Calculate 1st order
+    mean = math.mean(in_field).data
+    centered_field = in_field - mean
+    ret = solve_func(centered_field)
+    try:
+        solved_field, it = ret
+    except:
+        solved_field = ret
+    # Calculate 2nd order
+    ret2 = solve_func(solved_field)
+    try:
+        solved_field2, it = ret2
+    except:
+        solved_field2 = ret2
+    reconst2 = solved_field2.laplace().laplace() + mean
+    error2 = (in_field - reconst2)/in_field
+    max_error2 = np.max(np.abs(error2.data))
+    assert max_error2 < second_order_tolerance*set_accuracy, "{}^2 reconstruction not within set accuracy. {:.2g} vs. {:.2g}".format(name, max_error2, second_order_tolerance*set_accuracy)
+
+class TestReconstruction(TestCase):
+
+    def test_reconst(self, set_accuracy=1e-5, shape=[40, 32], first_order_tolerance=2, second_order_tolerance=20,
+                 boundary_list=[PERIODIC, OPEN, CLOSED]):
+        for boundary in boundary_list:
+            domain = Domain(shape, boundaries=boundary)
+            solver_list=[('SparseCG', lambda field: poisson_solve(field, domain, SparseCG(accuracy=set_accuracy))),
+                         ('GeometricCG', lambda field: poisson_solve(field, domain, GeometricCG(accuracy=set_accuracy))),
+                         ('SparseSciPy', lambda field: poisson_solve(field, domain, SparseSciPy())),
+                         ('FFT', math.fourier_poisson)]
+            in_data = CenteredGrid.sample(Noise(), domain)
+            sloped_data = (np.array([np.arange(shape[1]) for _ in range(shape[0])]).reshape(1, *shape, 1)/10+1)
+            in_data = in_data.copied_with(data=sloped_data)
+            for name, solver in solver_list:
+                print('Testing {} boundary with {} solver'.format(boundary, name))
+                test_reconstruction_first_order(in_data, solver, set_accuracy, name, first_order_tolerance=first_order_tolerance)
+                test_reconstruction_second_order(in_data, solver, set_accuracy, name, second_order_tolerance=second_order_tolerance)
+            print('Testing {} boundary with {} solver'.format(boundary, 'higher order FFT'))
+            run_second_order_fft_reconstruction(in_data, set_accuracy, second_order_tolerance=second_order_tolerance)
+
