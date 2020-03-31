2103e9c2d440ff1407df0021cb59274f27e872cf
Philipp <Philipp@MHoll.de>
2020-03-31 20:55:48 +0200

    Clean up code in test_object_math.py

--- tests/test_object_math.py
+++ tests/test_object_math.py
@@ -1,15 +1,9 @@
 from unittest import TestCase
 
 import numpy as np
-from phi import math
 
-from phi.flow import CLOSED, PERIODIC, OPEN, Domain, poisson_solve, Noise
-from phi.physics.pressuresolver.geom import GeometricCG
-from phi.physics.pressuresolver.sparse import SparseCG, SparseSciPy
-from phi.physics.pressuresolver.fourier import Fourier
+from phi.flow import CLOSED, PERIODIC, OPEN, Domain, Noise
 from phi.physics.field import CenteredGrid
-from phi.geom.geometry import AABox
-from phi.physics.field import Field
 
 
 class TestFieldMath(TestCase):
@@ -24,32 +18,28 @@ class TestFieldMath(TestCase):
             np.testing.assert_array_equal(result_array, 0)
 
     def test_addition_centered_grid(self):
-        """add one field from another"""
+        """add one field to another"""
         shape = [32, 27]
         for boundary in [CLOSED, PERIODIC, OPEN]:
             domain = Domain(shape, boundaries=(boundary, boundary))
-            centered_grid = CenteredGrid.sample(Noise(), domain)
-            centered_grid = centered_grid.copied_with(data=np.ones([1] + shape + [1]))
-            #centered_grid = Field(np.ones(shape)).sample_at(shape)#centered_grid.copied_with(data=np.ones(shape))
+            centered_grid = CenteredGrid.sample(1, domain)
             result_array = (centered_grid + centered_grid).data
             np.testing.assert_array_equal(result_array, 2)
 
     def test_multiplication_centered_grid(self):
-        """multiply one field from another"""
+        """multiply one field with another"""
         shape = [32, 27]
         for boundary in [CLOSED, PERIODIC, OPEN]:
             domain = Domain(shape, boundaries=(boundary, boundary))
-            centered_grid = CenteredGrid.sample(Noise(), domain)
-            centered_grid = centered_grid.copied_with(data=np.ones([1] + shape + [1]))
+            centered_grid = CenteredGrid.sample(1, domain)
             result_array = (centered_grid * centered_grid.copied_with(data=2*np.ones([1] + shape + [1]))).data
             np.testing.assert_array_equal(result_array, 2)
 
     def test_division_centered_grid(self):
-        """divide one field from another"""
+        """divide one field by another"""
         shape = [32, 27]
         for boundary in [CLOSED, PERIODIC, OPEN]:
             domain = Domain(shape, boundaries=(boundary, boundary))
-            centered_grid = CenteredGrid.sample(Noise(), domain)
-            centered_grid = centered_grid.copied_with(data=2*np.ones([1] + shape + [1]))
+            centered_grid = CenteredGrid.sample(2, domain)
             result_array = (centered_grid / centered_grid.copied_with(data=4*np.ones([1] + shape + [1]))).data
             np.testing.assert_array_equal(result_array, 1./2)
