Bug fixes: fp precision
- mostly caused by previous merge

phi/backend/scipy_backend.py
@@ -52,8 +52,9 @@ class SciPyBackend(Backend):
         else:
             array = np.array(x)
         # --- Enforce Precision ---
-        if array.dtype in (np.float16, np.float32, np.float64, np.longdouble) and self.has_fixed_precision:
-            array = self.to_float(array)
+        if not isinstance(array, numbers.Number):
+            if array.dtype in (np.float16, np.float32, np.float64, np.longdouble) and self.has_fixed_precision:
+                array = self.to_float(array)
         return array
 
     def is_tensor(self, x, only_native=False):

phi/tf/tf_backend.py
@@ -32,7 +32,7 @@ class TFBackend(Backend):
             return True
         return isinstance(x, (tf.Tensor, tf.Variable, tf.SparseTensor, tf.Operation))
 
-    def as_tensor(self, x):
+    def as_tensor(self, x, convert_external=True):
         if self.is_tensor(x, only_native=convert_external):
             tensor = x
         elif isinstance(x, np.ndarray):
@@ -40,8 +40,11 @@ class TFBackend(Backend):
         else:
             tensor = tf.convert_to_tensor(x)
         # --- Enforce Precision ---
-        if tensor.dtype.is_floating and self.has_fixed_precision:
-            tensor = self.to_float(tensor)
+        if not isinstance(tensor, numbers.Number):
+            if isinstance(tensor, np.ndarray):
+                tensor = SciPyBackend(precision=self.precision).as_tensor(tensor)
+            elif tensor.dtype.is_floating and self.has_fixed_precision:
+                tensor = self.to_float(tensor)
         return tensor
 
     def copy(self, tensor, only_mutable=False):

phi/torch/torch_backend.py
@@ -8,6 +8,7 @@ import torch.nn.functional as torchf
 
 from phi.backend.backend import Backend
 from phi.backend.backend_helper import split_multi_mode_pad, PadSettings, general_grid_sample_nd, combined_dim, symmetric_pad
+from phi.backend.scipy_backend import SciPyBackend
 
 
 class TorchBackend(Backend):
@@ -22,14 +23,12 @@ class TorchBackend(Backend):
     def is_tensor(self, x, only_native=False):
         if not only_native and isinstance(x, numbers.Number):
             return True
-
-    def is_tensor(self, x):
         return isinstance(x, (torch.Tensor, ComplexTensor))
 
     def as_tensor(self, x, convert_external=True):
         if self.is_tensor(x, only_native=convert_external):
             tensor = x
-            elif isinstance(x, np.ndarray):
+        elif isinstance(x, np.ndarray):
             tensor = torch.from_numpy(SciPyBackend(precision=self.precision).as_tensor(x))
         elif isinstance(x, (tuple, list)):
             try:
@@ -40,8 +39,9 @@ class TorchBackend(Backend):
         else:
             tensor = torch.tensor(x)
         # --- Enforce Precision ---
-        if tensor.dtype.is_floating_point and self.has_fixed_precision:
-            tensor = self.to_float(tensor)
+        if self.is_tensor(tensor, only_native=True):
+            if tensor.dtype.is_floating_point and self.has_fixed_precision:
+                tensor = self.to_float(tensor)
         return tensor
 
     def copy(self, tensor, only_mutable=False):