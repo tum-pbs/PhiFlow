d1e85e4f98e03d115617b151a8031d21e419cb24
Philipp Holl <philipp.holl@tum.de>
2020-10-26 14:47:52 +0100

    Document PINN demo

--- demos/PINN_burgers.py
+++ demos/PINN_burgers.py
@@ -16,8 +16,7 @@ def network(x, t):
     y = math.stack([x, t], axis=-1)
     for i in range(8):
         y = tf.layers.dense(y, 20, activation=tf.math.tanh, name='layer%d' % i, reuse=tf.AUTO_REUSE)
-    y = tf.layers.dense(y, 1, activation=None, name='layer_out', reuse=tf.AUTO_REUSE)
-    return y
+    return tf.layers.dense(y, 1, activation=None, name='layer_out', reuse=tf.AUTO_REUSE)
 
 
 def f(u, x, t):
@@ -50,9 +49,9 @@ with app.model_scope():
 # Physics loss
 x_ph, t_ph = tf.convert_to_tensor(rnd.random_uniform([1000], -1, 1)), tf.convert_to_tensor(rnd.random_uniform([1000], 0, 1))
 with app.model_scope():
-    loss_f = math.l2_loss(f(network(x_ph, t_ph)[:, 0], x_ph, t_ph))  # normalizes by first dimension, N_f
+    loss_ph = math.l2_loss(f(network(x_ph, t_ph)[:, 0], x_ph, t_ph))  # normalizes by first dimension, N_ph
 
-app.add_objective(loss_u, reg=loss_f)
+app.add_objective(loss_u, reg=loss_ph)  # allows us to control the influence of loss_ph as a slider in the interface
 
 # Display u on a grid
 grid_x, grid_t = [tf.convert_to_tensor(t, tf.float32) for t in np.meshgrid(np.linspace(-1, 1, 128), np.linspace(0, 1, 33), indexing='ij')]
@@ -61,7 +60,7 @@ with app.model_scope():
 app.add_field('u', grid_u)
 app.add_field('u0', grid_u[:, :, 0, :])
 
-app.action_reset = lambda: app.load_model(init_weights)
-app.prepare()
+app.action_reset = lambda: app.load_model(init_weights)  # Adds "Reset" button to the interface
+app.prepare()  # Initializes variables, adds controls
 init_weights = app.save_model()
-show(app)
+show(app)  # launches web interface
