b1a466342d01686179c7fde41367a4dbd72e238d
Philipp Holl <philipp.holl@tum.de>
2020-11-02 16:08:35 +0100

    Require Python >= 3.7

--- setup.py
+++ setup.py
@@ -267,8 +267,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
