4016f2e6dc468b9806cfe32cc2bb56703148238f
Robin <r.greif@outlook.com>
2020-11-02 18:35:48 +0100

    move legacy test_math to test__functions

--- tests/commit/math/test__functions.py
+++ tests/commit/math/test__functions.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from phi import math
+from phi.math import extrapolation
 
 
 def assert_not_close(*tensors, rel_tolerance, abs_tolerance):
@@ -31,3 +32,18 @@ class TestMathFunctions(TestCase):
         self.assertEqual(7, c.shape.b)
         math.assert_close(c.b[:3], 0)
         math.assert_close(c.b[3:], 1)
+
+    # TODO: Fix
+    def test_resample(self):
+        grid = math.sum(math.meshgrid([1, 2, 3], [0, 3]), 'vector')  # 1 2 3 | 4 5 6
+        coords = math.tensor([(0, 0), (0.5, 0), (0, 0.5), (-2, -1)], names=('list', 'vector'))
+        closest = math.closest_grid_values(grid, coords, extrapolation.ZERO)
+        interp = math.grid_sample(grid, coords, extrapolation.ZERO)
+        math.assert_close(interp, [1, 1.5, 2.5, 0])
+
+# Legacy test to be fixed
+# def _resample_test(mode, constant_values, expected):
+#    grid = np.tile(np.reshape(np.array([[1,2], [4,5]]), [1,2,2,1]), [1, 1, 1, 2])
+#    resampled = helper_resample(grid, coords, mode, constant_values, SciPyBackend())
+#    np.testing.assert_equal(resampled[..., 0], resampled[..., 1])
+#    np.testing.assert_almost_equal(expected, resampled[0, :, 0], decimal=5)
