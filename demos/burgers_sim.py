e1243261512c4a2fd52cb997f7b40037b2d9f394
Philipp Holl <philipp.holl@tum.de>
2020-11-14 17:04:13 +0100

    Adapt burgers_sim.py and simpleplume.py to use ModuleViewer

--- demos/burgers_sim.py
+++ demos/burgers_sim.py
@@ -2,23 +2,8 @@ from phi.flow import *
 
 
 domain = Domain(x=64, y=64, boundaries=PERIODIC, bounds=Box[0:100, 0:100])
-state = dict(velocity=domain.sgrid(Noise(vector=2)) * 2)
-# velocity = domain.vgrid(Noise(vector=2)) * 2
-# velocity = domain.sgrid(Sphere([50, 50], radius=15)) * [2, 0] + [1, 0]
+velocity = domain.grid(Noise(vector=2)) * 2
 
-# velocity.at_centers()
-
-
-def step(velocity: Grid, dt):
-    velocity = field.diffuse(velocity, 0.1, dt)
-    velocity = advect.semi_lagrangian(velocity, velocity, dt)
-    write_sim_frame(app.directory, velocity, app.frame, 'velocity')
-    return dict(velocity=velocity)
-
-
-app = App('Burgers Equation in %dD' % len(domain.resolution), framerate=5)
-
-state = step(dt=0, **state)
-
-app.set_state(state, step, show=['velocity'])
-show(app)
+for _ in ModuleViewer(framerate=10).range():
+    velocity = field.diffuse(velocity, 0.1, 1)
+    velocity = advect.semi_lagrangian(velocity, velocity, 1)
--- demos/simpleplume.py
+++ demos/simpleplume.py
@@ -1,23 +1,12 @@
 from phi.flow import *
 
-adv = advect.semi_lagrangian
-
 domain = Domain(x=64, y=80, boundaries=CLOSED, bounds=Box[0:100, 0:100])
 inflow = domain.grid(Sphere(center=(50, 10), radius=5)) * 0.2
-state = dict(velocity=domain.sgrid(0), density=domain.grid(0), pressure=domain.grid(0), divergence=domain.grid(0))
+velocity = domain.staggered_grid(0)
+density = pressure = divergence = domain.grid(0)
 
-
-def step(velocity, density, pressure, divergence, dt):
-    density = adv(density, velocity, dt) + inflow
+for _ in ModuleViewer(framerate=20).range():
+    density = advect.semi_lagrangian(density, velocity, 1) + inflow
     buoyancy_force = density * (0, 0.1) >> velocity
-    velocity = adv(velocity, velocity, dt) + buoyancy_force
+    velocity = advect.semi_lagrangian(velocity, velocity, 1) + buoyancy_force
     velocity, pressure, iterations, divergence = fluid.make_incompressible(velocity, domain, pressure_guess=pressure)
-    return dict(velocity=velocity, density=density, pressure=pressure, divergence=divergence)
-
-
-state = step(dt=5, **state)
-
-app = App('Simple Plume', framerate=10)
-app.set_state(state, step_function=step, dt=1.0, show=['velocity', 'density', 'divergence', 'pressure'])
-app.add_field('Remaining Divergence', lambda: field.divergence(app.state['velocity']))
-show(app, display=('density', 'velocity'))
