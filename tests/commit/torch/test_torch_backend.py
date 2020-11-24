3c6044fbc3a9aa06955a92180a2a84773d489e56
Robin <r.greif@outlook.com>
2020-11-24 20:15:11 +0100

    pytorch test pad()

--- tests/commit/torch/test_torch_backend.py
+++ tests/commit/torch/test_torch_backend.py
@@ -1,7 +1,14 @@
 from unittest import TestCase
+from phi.torch.flow import *
+import numpy as np
 
 
-class TestNothing(TestCase):
+class TestTorchBackend(TestCase):
 
-    def test_nothing(self):
-        pass
+    def test_pad(self):
+        grid_size = (32, 32)
+        a = np.random.rand(*grid_size)
+        a_np = np.pad(a, pad_width=1, mode='wrap')
+        #a_torch = math.pad(math.tensor(a, names=['x', 'y']), widths={'x': (1, 1), 'y': (1,1)}, mode=PERIODIC)
+        a_torch = TORCH_BACKEND.pad(torch.Tensor(a), pad_width=((1, 1), (1, 1)), mode='circular')
+        np.array_equal(a_np, a_torch)
