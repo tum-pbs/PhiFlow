10658191a122920b0c8e5786afa7ce62efcb5f0c
Philipp Holl <philipp.holl@tum.de>
2021-01-24 15:55:17 +0100

    Update demos
    * Update marker.py, waiting for PointCloud plotting from FLIP branch
    * Update smoke_plume.py
    * Remove loader_mantaflow.py

--- demos/marker.py
+++ demos/marker.py
@@ -1,34 +1,27 @@
-from phi.flow import *
+""" Passive Markers
 
-
-DESCRIPTION = """
 Fluid simulation with additional marker fields that are passively transported with the fluid.
 
 The dense marker is sampled on a regular grid while the sparse marker is a collection of particles.
 """
 
+from phi.flow import *
+
 
 def checkerboard(size=8, offset=2):
-    return math.to_float(math.all((domain.cells.center - offset) % (2 * size) < size, 'vector'))
+    return math.to_float(math.all((DOMAIN.cells.center - offset) % (2 * size) < size, 'vector'))
 
 
-domain = Domain([160, 126], CLOSED)
-velocity = domain.sgrid(Noise(vector=2, scale=100)) * 4
-dense_marker = CenteredGrid(checkerboard(), domain.bounds)
-points = math.join_dimensions(domain.cells.center.x[::4].y[::4], ('x', 'y'), 'points').points.as_batch()
+DOMAIN = Domain(x=126, y=160, boundaries=CLOSED)
+DT = 0.2
+
+velocity = DOMAIN.sgrid(Noise(vector=2, scale=100)) * 4
+dense_marker = CenteredGrid(checkerboard(), DOMAIN.bounds)
+points = math.join_dimensions(DOMAIN.cells.center.x[::4].y[::4], ('x', 'y'), 'points').points.as_batch()
 sparse_marker = PointCloud(Sphere(points, 1), math.random_normal(points.shape.without('vector')))
-state = dict(velocity=velocity, markers=(dense_marker, sparse_marker))
 
-
-def step(dt, velocity: CenteredGrid, markers: tuple):
-    velocity = advect.semi_lagrangian(velocity, velocity, dt)
-    velocity, _, _, _ = fluid.make_incompressible(velocity, domain)
-    markers = [advect.advect(m, velocity, dt) for m in markers]
-    return dict(velocity=velocity, markers=markers)
-
-
-app = App('Passive Markers', DESCRIPTION, framerate=10, dt=0.2)
-app.set_state(state, step, show=['velocity'])
-app.add_field('Dense Marker', lambda: app.state['markers'][0])
-app.add_field('Sparse Marker', lambda: domain.grid(app.state['markers'][1]))
-show(app, display=('Dense Marker', 'Sparse Marker'))
+for _ in ModuleViewer(framerate=10).range():
+    velocity = advect.semi_lagrangian(velocity, velocity, DT)
+    velocity, _, _, _ = fluid.make_incompressible(velocity, DOMAIN)
+    dense_marker = advect.advect(dense_marker, velocity, DT)
+    sparse_marker = advect.advect(sparse_marker, velocity, DT)
--- demos/smoke_plume.py
+++ demos/smoke_plume.py
@@ -1,17 +1,18 @@
-from phi.flow import *
+""" Smoke Plume
 
-# tf.compat.v1.disable_eager_execution()
+Hot smoke is emitted from a circular region at the bottom.
+The simulation computes the resulting air flow in a closed box.
+"""
+
+from phi.flow import *
 
 DOMAIN = Domain(x=80, y=80, boundaries=CLOSED, bounds=Box[0:100, 0:100])
 inflow = DOMAIN.grid(Sphere(center=(50, 10), radius=5)) * 0.2
-# velocity = DOMAIN.vgrid(0)
-velocity = DOMAIN.sgrid(0)
-smoke = pressure = divergence = remaining_divergence = DOMAIN.grid(0)
+velocity = DOMAIN.staggered_grid(0)  # alternatively vector_grid(0)
+smoke = pressure = divergence = DOMAIN.grid(0)
 
-for _ in range(20):
-# for _ in ModuleViewer(port=8050).range():
+for _ in ModuleViewer(display=('smoke', 'velocity')).range():
     smoke = advect.semi_lagrangian(smoke, velocity, 1) + inflow
     buoyancy_force = smoke * (0, 0.1) >> velocity  # resamples smoke to velocity sample points
     velocity = advect.semi_lagrangian(velocity, velocity, 1) + buoyancy_force
     velocity, pressure, iterations, divergence = fluid.make_incompressible(velocity, DOMAIN, pressure_guess=pressure)
-    remaining_divergence = field.divergence(velocity)
