22512b9cc4fb75bd811f080ba46ff407924aabeb
Philipp Holl <philipp.holl@tum.de>
2021-01-28 10:57:32 +0100

    Add ModuleViewer to API doc

--- phi/app/_module_app.py
+++ phi/app/_module_app.py
@@ -10,6 +10,17 @@ from ._display import show
 
 
 class ModuleViewer(App):
+    """
+    ModuleViewer launches the user interface to display the contents of the calling Python script.
+
+    Name and subtitle of the App may be specified in the module docstring (string before imports).
+    The first line is interpreted as the name, the rest as the subtitle.
+    If not specified, a generic name and description is chosen.
+
+    Use ModuleViewer.range() as a for-loop iteratable to control the loop execution from within the GUI.
+
+    Also see the user interface documentation at https://tum-pbs.github.io/PhiFlow/Web_Interface.html
+    """
     def __init__(self,
                  fields=None,
                  stride=None,
@@ -22,19 +33,12 @@ class ModuleViewer(App):
                  dt=1.0,
                  **show_config):
         """
-        ModuleViewer launches the user interface to display the contents of the calling Python script.
+        Creates the ModuleViewer `App` and `show()`s it.
 
-        Name and subtitle of the App may be specified in the module docstring (string before imports).
-        The first line is interpreted as the name, the rest as the subtitle.
-        If not specified, a generic name and description is chosen.
-
-        Use ModuleViewer.range() as a for-loop iteratable to control the loop execution from within the GUI.
-
-        Also see the user interface documentation at https://tum-pbs.github.io/PhiFlow/Web_Interface.html
-
-        :param fields: (Optional) names of global variables to be displayed.
-          If not provided, searches all global variables for Field or Tensor values.
-          All fields must exist as global variables before the ModuleViewer is instantiated.
+        Args:
+            fields: (Optional) names of global variables to be displayed.
+                If not provided, searches all global variables for Field or Tensor values.
+                All fields must exist as global variables before the ModuleViewer is instantiated.
         """
         module = inspect.getmodule(inspect.stack()[1].frame)
         doc = module.__doc__
@@ -59,6 +63,29 @@ class ModuleViewer(App):
         Thread(target=lambda: show(self, **show_config)).start()
 
     def range(self, *args):
+        """
+        Similar to `range()`, returns a generator that can be used in a `for` loop.
+
+            for step in ModuleViewer().range(100):
+                print(f'Running step {step}')
+
+        However, `ModuleViewer.range()` controlling the flow via the user interface.
+        Each element returned by the generator waits for `ModuleViewer.step()` to be invoked once.
+
+        Note that `step` is always equal to `ModuleViewer.step`.
+
+        This method can be invoked multiple times.
+        However, do not call this method while one `range` is still active.
+
+        Args:
+            *args:
+                No arguments for infinite loop.
+                `(stop: int)` to set number of iterations.
+                `(start: int, stop: int)` to additionally set initial value of `step`.
+
+        Returns:
+            generator yielding `ModuleViewer.step`
+        """
         if len(args) == 0:
             while True:
                 self.step_exec_event.wait()
@@ -82,6 +109,10 @@ class ModuleViewer(App):
             raise ValueError(args)
 
     def step(self):
+        """
+        Allows the generator returned by `ModuleViewer.range()` to advance one element.
+        In typical scenarios, this will run one loop iteration in the top-level script.
+        """
         self.step_finished_event.clear()
         self.step_exec_event.set()
         self.step_finished_event.wait()
