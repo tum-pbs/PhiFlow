7a6475507853b18d1d4c6332b4421e23ca39f9cb
JoKlim <klimesch.jonathan@gmail.com>
2021-01-31 11:55:41 +0100

    Small changes on demo.

--- demos/flip_liquid.py
+++ demos/flip_liquid.py
@@ -1,17 +1,21 @@
 from phi.flow import *
 
+# --- Setup environment ---
 inflow = 0
 domain = Domain(x=64, y=64, boundaries=CLOSED, bounds=Box[0:64, 0:64])
+obstacles = [Obstacle(Box[30:35, 30:35].rotated(math.tensor(-20)))]
+bcs = flip.get_bcs(domain, obstacles)
+
+# --- Initialize particles ---
 point_mask = domain.grid().values
 point_mask.native()[20:40, 50:60] = 1
 point_mask.native()[:, :10] = 1
 initial_points = distribute_points(point_mask, 8)
 initial_velocity = math.tensor(np.zeros(initial_points.shape), names=['points', 'vector'])
 initial_particles = PointCloud(Sphere(initial_points, 0), values=initial_velocity)
-obstacles = [Obstacle(Box[30:35, 30:35].rotated(math.tensor(-20)))]
-bcs = flip.get_bcs(domain, obstacles)
-state = dict(particles=initial_particles, v_field=initial_particles.at(domain.sgrid()), pressure=domain.grid(0), t=0,
-             cmask=PointCloud(initial_particles.elements).at(domain.grid()))
+
+state = dict(particles=initial_particles, v_field=initial_particles.at(domain.sgrid()), pressure=domain.grid(0),
+             t=0, cmask=PointCloud(initial_particles.elements).at(domain.grid()))
 
 
 def step(particles, v_field, pressure, dt, t, **kwargs):
@@ -20,7 +24,7 @@ def step(particles, v_field, pressure, dt, t, **kwargs):
     smask = points.at(domain.sgrid())
     v_force_field = flip.apply_gravity(dt, v_field)
     v_div_free_field, pressure = flip.make_incompressible(v_force_field, bcs, cmask, smask, pressure)
-    particles = flip.map2particle(particles, v_div_free_field, smask, v_field)
+    particles = flip.map2particle(particles, v_div_free_field, smask, initial_v_field=v_field)
     particles = advect.advect(particles, v_div_free_field, dt, mask=smask, bcs=bcs, mode='rk4_extp')
     if t < inflow:
         particles = flip.add_inflow(particles, initial_points, initial_velocity)
@@ -29,5 +33,5 @@ def step(particles, v_field, pressure, dt, t, **kwargs):
 
 
 app = App()
-app.set_state(state, step_function=step, dt=0.1, show=['particles', 'v_field', 'pressure', 'cmask'])
+app.set_state(state, step_function=step, dt=0.1, show=['v_field', 'pressure', 'cmask'])
 show(app, display='cmask')
