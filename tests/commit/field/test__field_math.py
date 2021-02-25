31cf067f3e4da104551baf0e02332e22a75bb80a
Philipp Holl <philipp.holl@tum.de>
2021-02-25 20:52:26 +0100

    Add unit test, update documentation

--- tests/commit/field/test__field_math.py
+++ tests/commit/field/test__field_math.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from phi import math
+from phi.field import StaggeredGrid, CenteredGrid
 from phi.geom import Box
 from phi import field
 from phi.physics import Domain
@@ -18,3 +19,17 @@ class TestFieldMath(TestCase):
         v = field.CenteredGrid(math.ones(x=3, y=3), Box[0:1, 0:1], math.extrapolation.ZERO) * (1, 0)  # flow to the right
         div = field.divergence(v).values
         math.assert_close(div.y[0], (1.5, 0, -1.5))
+
+    def test_trace_function(self):
+        def f(x: StaggeredGrid, y: CenteredGrid):
+            return x + (y >> x)
+
+        ft = field.trace_function(f)
+        domain = Domain(x=4, y=3)
+        x = domain.staggered_grid(1)
+        y = domain.vector_grid(1)
+
+        res_f = f(x, y)
+        res_ft = ft(x, y)
+        self.assertEqual(res_f.shape, res_ft.shape)
+        field.assert_close(res_f, res_ft)
