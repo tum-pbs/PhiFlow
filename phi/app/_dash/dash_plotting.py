6e53028576d39a0b861df1a98d2001e3d2fde8dc
Philipp Holl <philipp.holl@tum.de>
2021-03-01 18:00:51 +0100

    field.plot quiver support

--- phi/app/_dash/dash_plotting.py
+++ phi/app/_dash/dash_plotting.py
@@ -19,10 +19,8 @@ def dash_graph_plot(data, settings: dict) -> dict:
     try:
         if isinstance(data, np.ndarray):
             data = math.wrap(data)
-
         if isinstance(data, math.Tensor):
             data = CenteredGrid(data, Box(0, math.wrap(data.shape, 'vector')))
-
         if isinstance(data, (CenteredGrid, StaggeredGrid)):
             component = settings.get('component', 'x')
             if data.spatial_rank == 1:
@@ -37,11 +35,9 @@ def dash_graph_plot(data, settings: dict) -> dict:
                     return vector_field(slice_2d(data, settings), settings)
                 else:
                     return heatmap(slice_2d(data, settings), settings)
-
         if isinstance(data, PointCloud):
             return cloud_plot(data, settings)
-
-        warnings.warn('No figure recipe for data %s' % data)
+        warnings.warn(f"No figure recipe for {data}")
     except BaseException as err:
         print(f"Error during plotting: {err}")
     return EMPTY_FIGURE
--- phi/field/plt.py
+++ phi/field/plt.py
@@ -2,47 +2,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from phi import math
-from . import Grid
+from phi.geom import Box
+from . import Grid, CenteredGrid, StaggeredGrid
 from ._field import SampledField
 
 
-def plot(field: SampledField, title=False, colorbar=False, cmap='magma', figsize=(12, 5), same_scale=True):
-    if isinstance(field, Grid) and field.shape.channel.volume == 1:
-        return _plot_scalar_grid(field, title=title, colorbar=colorbar, cmap=cmap, figsize=figsize, same_scale=same_scale)
-    elif isinstance(field, Grid):
-        return _plot_vector_field(field, title=title, colorbar=colorbar, cmap=cmap, figsize=figsize)
-    raise NotImplementedError("Only scalar grids supported at this time.")
-    # im = axes[i].imshow(velocity_grad.inflow_loc[i].values.vector[0].numpy('y,x'), origin='lower', cmap='bwr')
-
-
-def _plot_scalar_grid(grid: Grid, title, colorbar, cmap, figsize, same_scale):
-    batch_size = grid.shape.batch.volume
-    values = math.join_dimensions(grid.values, grid.shape.channel, 'channel').channel[0]
-    plt_args = {}
-    if same_scale:
-        plt_args['vmin'] = math.min(values).native()
-        plt_args['vmax'] = math.max(values).native()
-    b_values = math.join_dimensions(values, grid.shape.batch, 'batch')
+def plot(field: SampledField, title=False, colorbar=False, cmap='magma', figsize=(12, 5), same_scale=True, **plt_args):
+    batch_size = field.shape.batch.volume
+    values = math.join_dimensions(field.values, field.shape.channel, 'channel').channel[0]
     fig, axes = plt.subplots(1, batch_size, figsize=figsize)
     axes = axes if isinstance(axes, np.ndarray) else [axes]
-    for b in range(batch_size):
-        im = axes[b].imshow(b_values.batch[b].numpy('y,x'), origin='lower', cmap=cmap, **plt_args)
-        if title:
+    b_values = math.join_dimensions(values, field.shape.batch, 'batch')
+    if title:
+        for b in range(batch_size):
             if isinstance(title, str):
                 sub_title = title
             elif title is True:
-                sub_title = f"{b} of {grid.shape.batch}"
+                sub_title = f"{b} of {field.shape.batch}"
             elif isinstance(title, (tuple, list)):
                 sub_title = title[b]
             else:
                 sub_title = None
             if sub_title is not None:
                 axes[b].set_title(sub_title)
-        if colorbar:
-            plt.colorbar(im, ax=axes[b])
+    # Individual plots
+    if isinstance(field, Grid) and field.shape.channel.volume == 1:
+        if same_scale:
+            plt_args['vmin'] = math.min(values).native()
+            plt_args['vmax'] = math.max(values).native()
+        for b in range(batch_size):
+            im = axes[b].imshow(b_values.batch[b].numpy('y,x'), origin='lower', cmap=cmap, **plt_args)
+            if colorbar:
+                plt.colorbar(im, ax=axes[b])
+    elif isinstance(field, Grid):
+        if isinstance(field, StaggeredGrid):
+            field = field.at_centers()
+        for b in range(batch_size):
+            x, y = field.points.vector.unstack_spatial('x,y', to_numpy=True)
+            data = math.join_dimensions(field.values, field.shape.batch, 'batch').batch[b]
+            u, v = data.vector.unstack_spatial('x,y', to_numpy=True)
+            axes[b].quiver(x-u/2, y-v/2, u, v)
+    else:
+        raise NotImplementedError(f"No figure recipe for {field}")
     plt.tight_layout()
     return fig, axes
-
-
-def _plot_vector_field(grid: Grid, title: bool, colorbar: bool, cmap: str, figsize):
-    raise NotImplementedError()
