328268d0a9a7c91b3d2e207703daae5f75b4234b
Philipp Holl <philipp.holl@tum.de>
2021-03-04 17:30:44 +0100

    [tests] Reset default backend after each backend test

--- tests/commit/test_demos.py
+++ tests/commit/test_demos.py
@@ -7,7 +7,7 @@ import numpy as np
 import phi.app._display as display
 from phi.app import ModuleViewer
 from phi.field import Field
-
+from phi.math import backend
 
 DEMOS_DIR = join(dirname(dirname(dirname(abspath(__file__)))), 'demos')
 
@@ -55,46 +55,46 @@ def demo_run(name):
         __import__(name)
     except InterruptedError:
         print(f'Test {name} successfully interrupted.')  # the demos are interrupted after a few steps
+    backend.set_global_default_backend(backend.NUMPY_BACKEND)
 
 
 class TestDemos(TestCase):
-    pass
 
-    # def test_burgers_sim(self):
-    #     demo_run('burgers_sim')
-    #
-    # def test_differentiate_pressure(self):
-    #     demo_run('differentiate_pressure')
-    #
-    # def test_flip_liquid(self):
-    #     demo_run('flip_liquid')
-    #
-    # def test_fluid_logo(self):
-    #     demo_run('fluid_logo')
-    #
-    # def test_heat_equilibrium(self):
-    #     demo_run('heat_equilibrium')
-    #
-    # def test_hw2d(self):
-    #     demo_run('hw2d')
-    #
-    # def test_marker(self):
-    #     demo_run('marker')
-    #
-    # def test_network_training_pytorch(self):
-    #     demo_run('network_training_pytorch')
-    #
-    # def test_pipe(self):
-    #     demo_run('pipe')
-    #
-    # def test_point_cloud(self):
-    #     demo_run('point_cloud')
-    #
-    # def test_profile_navier_stokes(self):
-    #     demo_run('profile_navier_stokes')
-    #
-    # def test_rotating_bar(self):
-    #     demo_run('rotating_bar')
-    #
-    # def test_smoke_plume(self):
-    #     demo_run('smoke_plume')
+    def test_burgers_sim(self):
+        demo_run('burgers_sim')
+
+    def test_differentiate_pressure(self):
+        demo_run('differentiate_pressure')
+
+    def test_flip_liquid(self):
+        demo_run('flip_liquid')
+
+    def test_fluid_logo(self):
+        demo_run('fluid_logo')
+
+    def test_heat_equilibrium(self):
+        demo_run('heat_equilibrium')
+
+    def test_hw2d(self):
+        demo_run('hw2d')
+
+    def test_marker(self):
+        demo_run('marker')
+
+    def test_network_training_pytorch(self):
+        demo_run('network_training_pytorch')
+
+    def test_pipe(self):
+        demo_run('pipe')
+
+    def test_point_cloud(self):
+        demo_run('point_cloud')
+
+    def test_profile_navier_stokes(self):
+        demo_run('profile_navier_stokes')
+
+    def test_rotating_bar(self):
+        demo_run('rotating_bar')
+
+    def test_smoke_plume(self):
+        demo_run('smoke_plume')
