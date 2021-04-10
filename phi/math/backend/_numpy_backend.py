[backend] Fix complex precision upon tensor creation

phi/math/backend/_numpy_backend.py
@@ -42,8 +42,10 @@ class NumPyBackend(Backend):
             array = np.array(x)
         # --- Enforce Precision ---
         if not isinstance(array, numbers.Number):
-            if array.dtype in (np.float16, np.float32, np.float64, np.longdouble):
+            if self.dtype(array).kind == float:
                 array = self.to_float(array)
+            elif self.dtype(array).kind == complex:
+                array = self.to_complex(array)
         return array
 
     def is_tensor(self, x, only_native=False):
@@ -336,7 +338,6 @@ class NumPyBackend(Backend):
             return np.fft.fftn(x, axes=list(range(1, rank + 1)))
 
     def ifft(self, k):
-        assert self.dtype(k).kind == complex
         rank = len(k.shape) - 2
         assert rank >= 1
         if rank == 1:

phi/tf/_tf_backend.py
@@ -36,10 +36,11 @@ class TFBackend(Backend):
         tf.random.set_seed(seed)
 
     def is_tensor(self, x, only_native=False):
+        is_tf_tensor = tf.is_tensor(x) is True  # tf.is_tensor() can return non-bool values which indicates not a Tensor
         if only_native:
-            return tf.is_tensor(x)
+            return is_tf_tensor
         else:
-            return tf.is_tensor(x) or NUMPY_BACKEND.is_tensor(x, only_native=False)
+            return is_tf_tensor or NUMPY_BACKEND.is_tensor(x, only_native=False)
 
     def as_tensor(self, x, convert_external=True):
         if self.is_tensor(x, only_native=convert_external):

phi/torch/_torch_backend.py
@@ -76,8 +76,11 @@ class TorchBackend(Backend):
             tensor = torch.tensor(x, device=self.get_default_device().ref)
         # --- Enforce Precision ---
         if self.is_tensor(tensor, only_native=True):
-            if tensor.dtype.is_floating_point:
+            dtype = self.dtype(tensor)
+            if dtype.kind == float:
                 tensor = self.to_float(tensor)
+            elif dtype.kind == complex:
+                tensor = self.to_complex(tensor)
         return tensor
 
     def is_available(self, tensor) -> bool: