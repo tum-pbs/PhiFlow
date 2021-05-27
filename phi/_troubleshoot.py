11e16db78c25e73896f292973aad4afb9ee2058f
Philipp Holl <philipp.holl@tum.de>
2021-05-27 19:54:11 +0200

    [math] Move count_tensors_in_memory to _troubleshoot.py

--- phi/_troubleshoot.py
+++ phi/_troubleshoot.py
@@ -138,12 +138,7 @@ def troubleshoot_dash():
 @contextmanager
 def plot_solves():
     """
-
-
     While `plot_solves()` is active, certain performance optimizations and algorithm implementations may be disabled.
-
-    Returns:
-
     """
     from . import math
     import pylab
@@ -174,3 +169,26 @@ def plot_solves():
             pylab.legend(loc='upper right')
             pylab.savefig(f"pressure-solvers-FP32.png")
             pylab.show()
+
+
+def count_tensors_in_memory(min_print_size: int = None):
+    import sys
+    import gc
+    from .math import Tensor
+
+    gc.collect()
+    total = 0
+    bytes = 0
+    for obj in gc.get_objects():
+        try:
+            if isinstance(obj, Tensor):
+                total += 1
+                size = obj.shape.volume * obj.dtype.itemsize
+                bytes += size
+                if isinstance(min_print_size, int) and size >= min_print_size:
+                    print(f"Tensor '{obj}' ({sys.getrefcount(obj)} references)")
+                    # referrers = gc.get_referrers(obj)
+                    # print([type(r) for r in referrers])
+        except Exception:
+            pass
+    print(f"There are {total} Φ-Tensors with a total size of {bytes / 1024 / 1024:.1f} MB")
