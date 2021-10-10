26c950002db9a83e6cb680ed801005e4662e669b
Marc Gavilán <marcgavilangil@gmail.com>
2021-10-10 16:22:44 +0200

    [math] Reset to changes to SmokePlume demo

--- demos/smoke_plume.py
+++ demos/smoke_plume.py
@@ -2,11 +2,8 @@
 Hot smoke is emitted from a circular region at the bottom.
 The simulation computes the resulting air flow in a closed box.
 """
-import os
-import threading
 
 from phi.torch.flow import *
-TORCH.set_default_device('GPU')
 
 DOMAIN = dict(x=64, y=64, bounds=Box[0:100, 0:100])
 INFLOW = CenteredGrid(Sphere(center=(50, 10), radius=5), extrapolation.BOUNDARY, **DOMAIN) * 0.2
@@ -19,6 +16,3 @@ for i in view(smoke, velocity, 'pressure', play=True, keep_alive=False).range(st
     buoyancy_force = smoke * (0, 0.1) >> velocity  # resamples smoke to velocity sample points
     velocity = advect.semi_lagrangian(velocity, velocity, 1) + buoyancy_force
     velocity, pressure = fluid.make_incompressible(velocity, (), Solve('auto', 1e-5, 0, x0=pressure))
-
-TORCH.close_profiler()
-os._exit(0)
