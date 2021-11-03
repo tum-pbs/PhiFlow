5c53a5f9c015a4e795030b32dd46ae18eea4114b
Philipp Holl <philipp.holl@tum.de>
2021-11-03 17:28:04 +0100

    [demos] Avoid memory leaks from retracing

--- demos/moving_obstacle.py
+++ demos/moving_obstacle.py
@@ -22,4 +22,5 @@ for _ in view(velocity, obstacle_mask, play=True, namespace=globals()).range():
     obstacle = move_obstacle(obstacle)
     velocity = advect.mac_cormack(velocity, velocity, DT)
     velocity, pressure = fluid.make_incompressible(velocity, (obstacle,))
+    fluid.masked_laplace.tracers.clear()  # we will need to retrace because the matrix changes each step. This is not needed when JIT-compiling the physics.
     obstacle_mask = HardGeometryMask(obstacle.geometry) @ pressure
