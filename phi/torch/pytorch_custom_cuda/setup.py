78f1d4db526572e40b684a8cc533c2732262d4bc
Marc Gavilán <marcgavilangil@gmail.com>
2021-11-04 08:37:42 +0100

    [math] Changes to setupy.py, compiler file for Torch C++ api

--- phi/torch/pytorch_custom_cuda/setup.py
+++ phi/torch/pytorch_custom_cuda/setup.py
@@ -2,16 +2,22 @@ from os import getcwd
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+CXX_ARGS = ["-L/home/marc/TUM/MyPhiFlow/venv/lib/python3.8/site-packages/torch/lib/"]
+NVCC_ARGS = ["-L/home/marc/TUM/MyPhiFlow/venv/lib/python3.8/site-packages/torch/lib/"]
+
+ext_modules = [
+    CUDAExtension(
+        name="pytorch_custom_cuda",
+        sources=["src/pytorch_custom.cpp", "src/cuda_code.cu"],
+        extra_compile_args={"cxx": CXX_ARGS, "nvcc": NVCC_ARGS},
+        libraries=["cusparse"],
+    ),
+]
+
 setup(
-    name='pytorch_custom_cuda',
-    ext_modules=[
-        CUDAExtension('pytorch_custom_cuda', [
-            'src/pytorch_custom.cpp',
-            ],
-            extra_compile_args={'nvcc': ['-lcublas', '-lcusparse']}
-        )
-    ],
-    include_dirs=[getcwd() + "/include/"],
-    cmdclass={
-        'build_ext': BuildExtension
-    })
+    name="pytorch_custom_cuda",
+    packages=[],
+    ext_modules=ext_modules,
+    include_dirs=["include"],
+    cmdclass={"build_ext": BuildExtension}
+)
