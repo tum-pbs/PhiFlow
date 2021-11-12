d8d77b3c6f2fd7aedff96cd91bb7fa9959204207
Philipp Holl <philipp.holl@tum.de>
2021-11-12 10:39:31 +0100

    [vis] Support smooth=-1 for constant curve in plot_scalars()

--- phi/vis/_plot_util.py
+++ phi/vis/_plot_util.py
@@ -4,7 +4,7 @@ import numpy
 def smooth_uniform_curve(x, values, n=16):
     if n == 1:
         return x, values
-    if len(x) <= n:
+    if n == -1 or len(x) <= n:
         mean = numpy.tile(numpy.mean(values, -1, keepdims=True), 2)
         return numpy.array([numpy.min(x), numpy.max(x)]), mean
     arrays = [values[i:i-n+1 or None] for i in range(n)]
