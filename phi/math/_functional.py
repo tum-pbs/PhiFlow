f00c2787bbb565e1cbcd83125970a2cda7fd5d20
Philipp Holl <philipp.holl@tum.de>
2022-04-12 21:00:37 +0200

    [math] Fix Solve.__eq__
    The function preprocess_y was not compared, resulting in incorrect traces or escaped tracers (Jax).

--- phi/math/_functional.py
+++ phi/math/_functional.py
@@ -1109,6 +1109,7 @@ class Solve(Generic[X, Y]):  # TODO move to phi.math._functional, put Tensors th
                 or (self.absolute_tolerance != other.absolute_tolerance).any \
                 or (self.relative_tolerance != other.relative_tolerance).any \
                 or (self.max_iterations != other.max_iterations).any \
+                or self.preprocess_y is not other.preprocess_y \
                 or self.suppress != other.suppress:
             return False
         return self.x0 == other.x0
