e2b21970abcc972bcd20bff6d8076eb1026cf3e7
Philipp Holl <philipp.holl@tum.de>
2022-04-12 20:59:38 +0200

    [demos] Make smoke_plume.py jit-compatible
    * Remove smoke_plume_jit.py

--- demos/smoke_plume_3d.py
+++ demos/smoke_plume_3d.py
@@ -2,23 +2,26 @@
 Hot smoke is emitted from a circular region at the bottom.
 The simulation computes the resulting air flow in a closed box.
 """
+from phi.flow import *  # minimal dependencies
+# from phi.torch.flow import *
+# from phi.tf.flow import *
+# from phi.jax.flow import *
 
-from phi.flow import *
 
-DOMAIN = dict(x=32, y=32, z=32, bounds=Box(x=100, y=100, z=100))
-velocity = StaggeredGrid(0, extrapolation.ZERO, **DOMAIN)  # or use CenteredGrid
-smoke = CenteredGrid(0, extrapolation.BOUNDARY, **DOMAIN)
-INFLOW = 0.2 * CenteredGrid(SoftGeometryMask(Sphere(x=50, y=50, z=10, radius=5)), extrapolation.ZERO, **DOMAIN)
+velocity = StaggeredGrid((0, 0, 0), extrapolation.ZERO, x=32, y=32, z=32, bounds=Box(x=100, y=100, z=100))  # or CenteredGrid(...)
+smoke = CenteredGrid(0, extrapolation.BOUNDARY, x=32, y=32, z=32, bounds=Box(x=100, y=100, z=100))
+INFLOW = 0.2 * CenteredGrid(SoftGeometryMask(Sphere(x=50, y=50, z=10, radius=5)), 0, smoke.bounds, smoke.resolution)
 pressure = None
 
 
-def step(smoke, velocity, pressure):
-    smoke = advect.mac_cormack(smoke, velocity, 1) + INFLOW
-    buoyancy_force = smoke * (0, 0, 0.1) @ velocity  # resamples smoke to velocity sample points
-    velocity = advect.semi_lagrangian(velocity, velocity, 1) + buoyancy_force
-    velocity, pressure = fluid.make_incompressible(velocity, (), Solve('auto', 1e-3, 0, x0=pressure))
-    return smoke, velocity, pressure
+# @math.jit_compile  # Only for PyTorch, TensorFlow and Jax
+def step(v, s, p, dt=1.):
+    s = advect.mac_cormack(s, v, dt) + INFLOW
+    buoyancy = s * (0, 0, 0.1) @ v  # resamples smoke to velocity sample points
+    v = advect.semi_lagrangian(v, v, dt) + buoyancy * dt
+    v, p = fluid.make_incompressible(v, (), Solve('auto', 1e-5, 0, x0=p))
+    return v, s, p
 
 
 for _ in view(smoke, velocity, 'pressure', play=False, namespace=globals()).range(warmup=1):
-    smoke, velocity, pressure = step(smoke, velocity, pressure)
+    velocity, smoke, pressure = step(velocity, smoke, pressure)
