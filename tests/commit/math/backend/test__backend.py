ce18e806d56c9b7680d9187e38cbcd008b268bbd
Philipp Holl <philipp.holl@tum.de>
2022-04-25 14:05:53 +0200

    [tests] Fix test__backend.py

--- tests/commit/math/backend/test__backend.py
+++ tests/commit/math/backend/test__backend.py
@@ -20,7 +20,7 @@ class TestBackends(TestCase):
     def test_convert(self):  # TODO this causes RuntimeError when GPU capsule is given to Jax in CPU mode
         for source_backend in BACKENDS:
             for target_backend in BACKENDS:
-                data = source_backend.random_normal([4])  # may be deleted in conversion
+                data = source_backend.random_normal([4], None)  # may be deleted in conversion
                 print(f"{source_backend} -> {target_backend}   {data}")
                 converted = convert(data, target_backend)
                 np1 = source_backend.numpy(data)
