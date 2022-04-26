8fde1d77fabc1939d7157bf51ea97fb2454a055c
Philipp Holl <philipp.holl@tum.de>
2022-04-26 15:12:36 +0200

    [demos] Rename & update burgers.py

--- demos/burgers.py
+++ demos/burgers.py
@@ -6,6 +6,13 @@ from phi.flow import *
 
 velocity = CenteredGrid(Noise(vector=2), extrapolation.PERIODIC, x=64, y=64, bounds=Box(x=200, y=100)) * 2
 
+
+# @math.jit_compile  # for PyTorch, TensorFlow and Jax
+def burgers_step(v, dt=1.):
+    v = diffuse.explicit(v, 0.1, dt=dt)
+    v = advect.semi_lagrangian(v, v, dt=dt)
+    return v
+
+
 for _ in view(play=False, framerate=10, namespace=globals()).range():
-    velocity = diffuse.explicit(velocity, 0.1, dt=1)
-    velocity = advect.semi_lagrangian(velocity, velocity, dt=1)
+    velocity = burgers_step(velocity)
