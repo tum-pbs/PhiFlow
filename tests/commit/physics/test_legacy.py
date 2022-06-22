c0e7993cbc2a82a7a341953473e1c57b0b00094a
holl- <philipp@mholl.de>
2022-06-22 12:36:40 +0200

    [physics] Bring back Domain with more deprecation warnings.

---
+++ tests/commit/physics/test_legacy.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from phi import struct, math
+from phi.geom import Sphere, Box
+from phi.math import batch, channel
+from phi.physics._boundaries import Domain, Obstacle
+
+
+class TestLegacyPhysics(TestCase):
+
+    def test_domain(self):
+        dom = Domain(x=16, y=16)
+        dom.scalar_grid(0)
+
