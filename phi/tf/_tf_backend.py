[math] Fix PyTorch custom gradient + jit
Fixes an error where the output tree of a custom gradient function was not recorded since PyTorch skips some function calls to avoid double execution. This is now cached in advance.

phi/tf/_tf_backend.py
@@ -107,7 +107,7 @@ class TFBackend(Backend):
         compiled = tf.function(f)
         return lambda *args: self.as_registered.call(compiled, *args, name=f"run jit-compiled '{f.__name__}'")
 
-    def custom_gradient(self, f: Callable, gradient: Callable = None) -> Callable:
+    def custom_gradient(self, f: Callable, gradient: Callable, get_external_cache: Callable = None, on_call_skipped: Callable = None) -> Callable:
         @tf.custom_gradient
         def tf_function(*args, **kwargs):
             def grad(*grad_args):