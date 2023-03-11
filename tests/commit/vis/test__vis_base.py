bce698377eaec7b1afbed797b0839d401fb4f3fa
Philipp Holl <philipp.holl@tum.de>
2023-03-11 20:32:38 +0100

    [vis] Bar charts

--- tests/commit/vis/test__vis_base.py
+++ tests/commit/vis/test__vis_base.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from phi import math
 from phi.field import CenteredGrid, PointCloud
 from phi.math import spatial, channel, instance
-from phi.vis._vis_base import tensor_as_field
+from phi.vis._vis_base import to_field
 
 
 class TestVisBase(TestCase):
@@ -11,15 +11,15 @@ class TestVisBase(TestCase):
     def test_tensor_as_field(self):
         # --- Grid ---
         t = math.random_normal(spatial(x=4, y=3), channel(vector='x,y'))
-        grid = tensor_as_field(t)
+        grid = to_field(t)
         self.assertIsInstance(grid, CenteredGrid)
         math.assert_close(grid.dx, 1)
         math.assert_close(grid.points.x[0].y[0], 0)
         # --- PointCloud ---
         t = math.random_normal(instance(points=5), channel(vector='x,y'))
-        points = tensor_as_field(t)
+        points = to_field(t)
         self.assertIsInstance(points, PointCloud)
         # --- Arbitrary lines ---
         t = math.random_normal(spatial(points=5), channel(vector='x,y'))
-        points = tensor_as_field(t)
+        points = to_field(t)
         self.assertIsInstance(points, PointCloud)
