b827a83876c886b42ea88a7b30987c44eeb2ea32
Philipp Holl <philipp.holl@tum.de>
2023-04-01 16:10:59 +0200

    [math] Sparse improvements
    * Add map_pairs()
    * Add is_sparse()
    * Add tensor_like()
    * Add default to sparse tensors
    * Fix decompress() for sliced compressed matrices
    * Add format to sparse_tensor()
    * Add stored_values(), stored_indices()
    * Basic sparse COO slicing
    * Fix sparse stack

--- tests/commit/math/test__sparse.py
+++ tests/commit/math/test__sparse.py
@@ -14,12 +14,12 @@ class TestSparse(TestCase):
         self.assertEqual(1, get_sparsity(wrap(1)))
         self.assertEqual(0.25, get_sparsity(expand(1., batch(b=4))))
         self.assertEqual(0.25, get_sparsity(stack([zeros(batch(b=4))] * 3, channel('vector'))))
-        self.assertEqual(0.3, get_sparsity(SparseCoordinateTensor(ones(instance(nnz=3), channel(vector='x'), dtype=int), ones(instance(nnz=3)), spatial(x=10), True, False)))
+        self.assertEqual(0.3, get_sparsity(SparseCoordinateTensor(ones(instance(nnz=3), channel(vector='x'), dtype=int), ones(instance(nnz=3)), spatial(x=10), True, False, default=0)))
         self.assertEqual(0.03, get_sparsity(CompressedSparseMatrix(indices=ones(instance(nnz=3), dtype=int),
                                                                    pointers=ones(instance(y_pointers=11), dtype=int),
                                                                    values=ones(instance(nnz=3)),
                                                                    uncompressed_dims=spatial(x=10),
-                                                                   compressed_dims=spatial(y=10))))
+                                                                   compressed_dims=spatial(y=10), default=0)))
 
     def test_csr(self):
         for backend in BACKENDS:
@@ -27,7 +27,7 @@ class TestSparse(TestCase):
                 indices = tensor([0, 1, 0], instance('nnz'))
                 pointers = tensor([0, 2, 3, 3], instance('pointers'))
                 values = tensor([2, 3, 4], instance('nnz'))
-                matrix = CompressedSparseMatrix(indices, pointers, values, channel(right=3), channel(down=3))
+                matrix = CompressedSparseMatrix(indices, pointers, values, channel(right=3), channel(down=3), 0)
                 math.print(dense(matrix))
                 assert_close((2, 3, 0), dense(matrix).down[0])
                 assert_close((4, 0, 0), dense(matrix).down[1])
@@ -39,7 +39,7 @@ class TestSparse(TestCase):
 
     def test_csr_slice_concat(self):
         pos = tensor([(0, 0), (0, 1), (0, 2)], instance('particles'), channel(vector='x,y'))
-        dx = math.pairwise_distances(pos, max_distance=1.5, format='csr')
+        dx = pairwise_distances(pos, max_distance=1.5, format='csr')
         self.assertEqual(0, dx.sum)
         dist = math.vec_length(dx, eps=1e-6)
         self.assertEqual(set(dual(particles=3) & instance(particles=3)), set(dist.shape))
