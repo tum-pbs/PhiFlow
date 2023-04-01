dc7a78363aaf896c9a74a1076581fd5119bc59dd
Philipp Holl <philipp.holl@tum.de>
2023-04-01 16:10:59 +0200

    [math] Sparse improvements
    * Add map_pairs()
    * Add is_sparse()
    * Add tensor_like()
    * Add default to sparse tensors
    * Fix decompress() for sliced compressed matrices
    * Add format to sparse_tensor()
    * Add stored_values(), stored_indices()
    * Basic sparse COO slicing
    * Fix sparse stack

--- phi/math/__init__.py
+++ phi/math/__init__.py
@@ -34,7 +34,7 @@ from ._magic_ops import (
 
 from ._tensors import wrap, tensor, layout, Tensor, Dict, to_dict, from_dict, is_scalar, BROADCAST_FORMATTER as f
 
-from ._sparse import dense, get_sparsity, factor_ilu, get_format, sparse_tensor
+from ._sparse import dense, get_sparsity, factor_ilu, get_format, sparse_tensor, stored_indices, stored_values, tensor_like
 
 from .extrapolation import Extrapolation
 
@@ -66,7 +66,7 @@ from ._ops import (
     dtype, cast,
     close, assert_close,
     stop_gradient,
-    pairwise_distances,
+    pairwise_distances, map_pairs,
 )
 
 from ._trace import matrix_from_function
