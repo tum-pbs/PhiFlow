99ba0920e8744e753b33ada98432f87a6241cfa9
Philipp Holl <philipp.holl@tum.de>
2023-04-16 14:44:17 +0200

    [math] Update Backend.ravel_multi_index()

--- phi/math/backend/_numpy_backend.py
+++ phi/math/backend/_numpy_backend.py
@@ -255,9 +255,14 @@ class NumPyBackend(Backend):
     def unravel_index(self, flat_index, shape):
         return np.stack(np.unravel_index(flat_index, shape), -1)
 
-    def ravel_multi_index(self, multi_index, shape, wrap=False):
-        idx_first = np.swapaxes(multi_index, 0, -1)
-        return np.ravel_multi_index(idx_first, shape, mode='wrap' if wrap else 'raise')
+    def ravel_multi_index(self, multi_index, shape, mode: Union[str, int] = 'undefined'):
+        mode = mode if isinstance(mode, int) else {'undefined': 'raise', 'periodic': 'wrap', 'clamp': 'clip'}[mode]
+        idx_first = np.transpose(multi_index, (-1,) + tuple(range(self.ndims(multi_index)-1)))
+        result = np.ravel_multi_index(idx_first, shape, mode='wrap' if isinstance(mode, int) else mode)
+        if isinstance(mode, int):
+            outside = self.any((multi_index < 0) | (multi_index >= shape), -1)
+            result = self.where(outside, mode, result)
+        return result
 
     def gather(self, values, indices, axis: int):
         slices = [indices if i == axis else slice(None) for i in range(self.ndims(values))]
