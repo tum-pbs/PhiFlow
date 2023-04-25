474b48df49ac908c9af0405344f54fe03a19c616
Philipp Holl <philipp.holl@tum.de>
2023-04-25 14:02:38 +0200

    [math] Update neighbor search

--- phi/math/backend/_partition.py
+++ phi/math/backend/_partition.py
@@ -1,36 +1,30 @@
 from numbers import Number
-from typing import Tuple, Any
+from typing import Tuple, Any, Union, Optional
 
 import numpy as np
 
 from ._dtype import DType, to_numpy_dtype
-from ._backend import Backend
+from ._backend import Backend, choose_backend, TensorType
 
 
-def neighbor_search(b: Backend,
-                    positions,
-                    max_dist,
-                    tmp_pair_count: int = None,
-                    pair_count: int = None,
-                    table_len: int = None,
-                    index_dtype: DType = DType(int, 32),
-                    trim: bool = True,
-                    default: Number = float('nan'),
-                    cell_mode: str = 'grid',
-                    pair_by: str = 'gather-search') -> tuple:
+def find_neighbors(positions,
+                   cutoff,
+                   domain: Optional[Tuple[TensorType, TensorType]] = None,
+                   periodic: Union[bool, Tuple[bool, ...]] = False,
+                   pair_count: int = None,
+                   index_dtype: DType = DType(int, 32),
+                   trim: bool = True,
+                   default: Number = float('nan'),
+                   cell_mode: str = 'grid',
+                   pair_by: str = 'gather-search') -> tuple:
     """
 
     Args:
-        b: `Backend`
         positions: Point locations of shape (batch, instances, vector)
-        max_dist: Scalar float
-        tmp_pair_count: Maximum number of total particle pairs, including ones in neighboring cells that are more than `max_dist` apart.
-            This must be set when jit-compiling this function with Jax.
+        cutoff: Scalar float or 1D tensor
         pair_count: Length of output lists.
             This must be set when jit-compiling this function with Jax.
-        table_len: Maximum lookup table size. For `cell_mode='grid'`, this equals the maximum number of virtual cells.
-            This must be set when jit-compiling this function with Jax.
-        index_dtype: Either int32 or int64
+        index_dtype: Either int32 or int64.
         trim: Whether to only include pairs for particles in range. If `False`, returns a possibly larger list with invalid deltas represented as `default´.
             If `pair_count` is specified the result list may be of that size and fill unused values with `default` anyway.
         default: Value to return for particles that are further apart than `max_dist` or otherwise invalid values of `differences`.
@@ -42,11 +36,12 @@ def neighbor_search(b: Backend,
         target_indices: (pair_count,)
         differences: (pair_count, ndims)
     """
-    assert b.ndims(max_dist) == 0, f"Only scalar max_distance currently supported for sparse distances"
+    b = choose_backend(positions)
     positions = b.to_float(b.as_tensor(positions))
-    max_dist = b.to_float(max_dist)
+    cutoff = b.to_float(cutoff)
     num_part, d = b.staticshape(positions)
-    cells, perm, neighbor_cells, structure = build_cells(b, positions, max_dist, cell_mode, table_len=table_len)
+    cells, perm, neighbor_cells, cell_size, structure = build_cells(b, positions, cutoff, domain, periodic, cell_mode)
+    tmp_pair_count = pair_count * b.prod(3 * cell_size) / (_sphere_volume(cutoff, d)) if pair_count is not None else None  # approximate over-counting from including out-of-range particles in neighboring cells
     linear_indices = b.range(num_part, dtype=index_dtype)
     particle_ids = b.gather(linear_indices, perm, 0)
     positions = b.gather(positions, perm, 0)
@@ -54,7 +49,7 @@ def neighbor_search(b: Backend,
     num_neighbors_by_direction = structure.get_num_elements_in_cell(neighbor_cells)
     num_potential_neighbors = b.sum(num_neighbors_by_direction, 0)
     num_required_tmp_pairs = b.sum(num_potential_neighbors)
-    long_linear_indices = b.range(tmp_pair_count or num_required_tmp_pairs, dtype=index_dtype)
+    pair_indices = b.range(tmp_pair_count or num_required_tmp_pairs, dtype=index_dtype)
     if pair_by == 'scatter':
         raise NotImplementedError
         # to_id = b.zeros(sum(num_potential_neighbors), dtype=index_dtype)  # ToDo bound this by some constant (jit)
@@ -64,9 +59,8 @@ def neighbor_search(b: Backend,
         #     offsets += ...
     elif pair_by == 'gather-search':
         neighbor_partitions = b.cumsum(num_neighbors_by_direction, 0)
-        # first_in_cell = b.repeat(structure.get_first_element_by_cell(cells), num_potential_neighbors, 0)
         small_index = b.repeat(linear_indices, num_potential_neighbors, 0, new_length=tmp_pair_count)
-        nb_part_idx_in_cell = long_linear_indices - b.repeat(b.cumsum(num_potential_neighbors, 0) - num_potential_neighbors, num_potential_neighbors, 0, new_length=tmp_pair_count)
+        nb_part_idx_in_cell = pair_indices - b.repeat(b.cumsum(num_potential_neighbors, 0) - num_potential_neighbors, num_potential_neighbors, 0, new_length=tmp_pair_count)
         neighbor_partitions = b.repeat(b.transpose(neighbor_partitions, (1, 0)), num_potential_neighbors, 0, new_length=tmp_pair_count)
         direction = b.searchsorted(neighbor_partitions, nb_part_idx_in_cell[:, None], 'right')[:, 0]
         neighbor_cell = b.gather_by_component_indices(neighbor_cells, direction, small_index)
@@ -80,10 +74,10 @@ def neighbor_search(b: Backend,
     from_positions = b.repeat(positions, num_potential_neighbors, 0, new_length=tmp_pair_count)
     to_positions = b.gather(positions, to_idx, 0)
     dx = to_positions - from_positions
-    # --- Filter by max_dist ---
+    # --- Filter by cutoff ---
     dist = b.sqrt(b.sum(dx ** 2, -1))
-    in_range = dist < max_dist
-    valid = in_range & (long_linear_indices < num_required_tmp_pairs)
+    in_range = dist < cutoff
+    valid = in_range & (pair_indices < num_required_tmp_pairs)
     if trim and (tmp_pair_count is None or (pair_count is not None and pair_count < tmp_pair_count)):
         pair_count = pair_count or tmp_pair_count
         dx = b.boolean_mask(dx, valid, new_length=pair_count, fill_value=default)
@@ -96,25 +90,37 @@ def neighbor_search(b: Backend,
     return from_id, to_id, dx
 
 
-def build_cells(b: Backend, positions, min_cell_size, mode: str, table_len: int = None) -> Tuple[Any, Any, Any, 'IndexingStructure']:
+def build_cells(b: Backend,
+                positions,
+                min_cell_size,
+                domain: Optional[Tuple[TensorType, TensorType]],
+                periodic: Union[bool, Tuple[bool, ...]],
+                mode: str) -> Tuple[TensorType, TensorType, TensorType, TensorType, 'IndexingStructure']:
     assert mode in ['grid', 'tree'], mode  # , 'hexagonal-grid'
-    num_part, d = b.staticshape(positions)
-    lower = b.min(positions, 0)
-    upper = b.max(positions, 0) + 1e-5
-    extent = b.maximum(upper - lower, 1e-15)
+    _, d = b.staticshape(positions)
+    periodic = [periodic] * d if np.ndim(periodic) == 0 else tuple(periodic)
+    if domain is None:
+        domain = b.min(positions, 0), b.max(positions, 0) + 1e-5
+    domain_size = b.maximum(domain[1] - domain[0], min_cell_size)
     if mode == 'grid':
-        resolution = b.maximum(1, b.to_int32(extent / min_cell_size))  # ToDo decrease resolution if larger than table_len
-        cell_indices = b.to_int32((positions - lower) / extent * b.to_float(resolution))
+        resolution = b.maximum(1, b.to_int32(b.ceil(domain_size / min_cell_size)))
+        cell_count = b.prod(resolution)
+        extent = min_cell_size * resolution
+        cell_size = extent / resolution
+        cell_indices = b.to_int32((positions - domain[0]) / extent * b.to_float(resolution))
         cell_ids = b.ravel_multi_index(cell_indices, resolution)
         perm = b.argsort(cell_ids)
         cell_indices = b.gather(cell_indices, perm, 0)
         cell_ids = b.gather(cell_ids, perm, 0)
-        num_virtual_cells = b.prod(resolution, 0)
-        idx_by_cell = b.searchsorted(cell_ids, b.range(table_len or num_virtual_cells), 'left')
-        len_by_cell = b.bincount(cell_ids, None, bins=num_part)
+        idx_by_cell = b.searchsorted(cell_ids, b.range(cell_count), 'left')
+        len_by_cell = b.bincount(cell_ids, None, bins=cell_count)
         neighbor_offsets = b.as_tensor(np.reshape(np.stack(np.meshgrid(*[(-1, 0, 1)] * d, indexing='ij'), -1), (-1, 1, d)))
-        neighbor_ids = b.ravel_multi_index(cell_indices + neighbor_offsets, resolution, mode=-1)
-        return cell_ids, perm, neighbor_ids, CellArray(b, idx_by_cell, len_by_cell)
+        if any(periodic):
+            assert all(periodic), f"Only fully periodic or fully non-periodic boundaries currently supported"
+            neighbor_ids = b.ravel_multi_index(cell_indices + neighbor_offsets, resolution, mode='periodic')
+        else:
+            neighbor_ids = b.ravel_multi_index(cell_indices + neighbor_offsets, resolution, mode=-1)
+        return cell_ids, perm, neighbor_ids, cell_size, CellArray(b, idx_by_cell, len_by_cell)
     raise NotImplementedError(mode)
 
 
@@ -146,32 +152,33 @@ class CellTree(IndexingStructure):
     ...
 
 
-class CellHashMap(IndexingStructure):
-    ...
+def _sphere_volume(radius, d: int):
+    if d == 1:
+        return 2 * radius
+    elif d == 2:
+        return np.pi * radius ** 2
+    elif d == 3:
+        return 4 / 3 * np.pi * radius ** 3
+    raise NotImplementedError(f"d={d}")
 
 
 # ToDo hierarchy of cells if too many cells to list all explicitly. Sorting by lowest index must also sort for higher indices
 
 
-def pairwise_distances_sklearn(self: Backend, positions, max_dist, index_dtype=DType(int, 32)):
-    from sklearn import neighbors
-    batch_size, point_count, _vec_count = self.staticshape(positions)
-    positions_np_batched = self.numpy(positions)
-    result = []
-    for i in range(batch_size):
-        tree = neighbors.KDTree(positions_np_batched[i])
-        radius = float(max_dist) if len(self.staticshape(max_dist)) == 0 else max_dist[i]
-        nested_neighbors = tree.query_radius(positions_np_batched[i], r=radius)  # ndarray[ndarray]
-        column_indices = np.concatenate(nested_neighbors).astype(to_numpy_dtype(index_dtype))  # flattened_neighbors
-        neighbor_counts = [len(nlist) for nlist in nested_neighbors]
-        row_pointers = np.concatenate([[0], np.cumsum(neighbor_counts)]).astype(to_numpy_dtype(index_dtype))
-        pos_neighbors = self.gather(positions[i], column_indices, 0)
-        pos_self = self.repeat(positions[i], neighbor_counts, axis=0)
-        values = pos_neighbors - pos_self
-        result.append((column_indices, row_pointers, values))
-        # sparse_matrix = self.csr_matrix(column_indices, row_pointers, values, (point_count, point_count))
-        # sparse_matrix.eliminate_zeros()  # setdiag(0) keeps zero entries
-    return result
+def find_neighbors_sklearn(positions: TensorType,
+                           cutoff: Union[float, TensorType],
+                           index_dtype=DType(int, 32)):
+    from sklearn.neighbors import KDTree
+    b = choose_backend(positions)
+    np_positions = b.numpy(positions)
+    tree = KDTree(np_positions)
+    nested_neighbors = tree.query_radius(np_positions, r=cutoff)  # ndarray[ndarray]
+    col = np.concatenate(nested_neighbors).astype(to_numpy_dtype(index_dtype), copy=False)  # flattened_neighbors
+    neighbor_counts = [len(nlist) for nlist in nested_neighbors]
+    row_pointers = np.concatenate([[0], np.cumsum(neighbor_counts)]).astype(to_numpy_dtype(index_dtype), copy=False)
+    pos_neighbors = b.gather(positions, col, 0)
+    pos_self = b.repeat(positions, neighbor_counts, axis=0)
+    return col, row_pointers, pos_neighbors - pos_self
 
 
 def find_neighbors_matscipy(positions: TensorType,
@@ -219,3 +226,21 @@ def find_neighbors_matscipy(positions: TensorType,
     col = b.concat([self_indices, col], 0)
     deltas = b.concat([self_deltas, deltas], 0)
     return row, col, deltas
+
+
+# def find_neighbors_jax_md(positions: TensorType,
+#                           cutoff: Union[float, TensorType],
+#                           domain: Optional[Tuple[TensorType, TensorType]] = None,
+#                           periodic: Union[bool, Tuple[bool, ...]] = False,
+#                           index_dtype=DType(int, 32)):
+#     from jax_md import space, partition
+#     displacement_fn, shift_fn = space.periodic(1.0)
+#     jaxmd_nl_fn = partition.neighbor_list(
+#         displacement_fn,
+#         box=box,
+#         r_cutoff=cutoffs[i],
+#         format=partition.NeighborListFormat.Sparse,
+#         capacity_multiplier=1.0
+#     )
+#     jaxmd_nl = jaxmd_nl_fn.allocate(poss[i])
+#     jaxmd_nl = jaxmd_nl.update(poss[i])
