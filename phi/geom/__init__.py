678b184506cb4938d01622485d64212ee4e18840
holl- <philipp@mholl.de>
2023-05-26 14:54:43 +0200

    [geom] Add UniformGrid alias

--- phi/geom/__init__.py
+++ phi/geom/__init__.py
@@ -12,7 +12,7 @@ See the `phi.geom` module documentation at https://tum-pbs.github.io/PhiFlow/Geo
 from ..math import stack, concat, pack_dims  # for compatibility
 from ._geom import Geometry, Point, assert_same_rank, invert
 from ._union import union
-from ._box import Box, GridCell, BaseBox, Cuboid
+from ._box import Box, GridCell, GridCell as UniformGrid, BaseBox, Cuboid
 from ._sphere import Sphere
 from ._transform import embed, infinite_cylinder
 
