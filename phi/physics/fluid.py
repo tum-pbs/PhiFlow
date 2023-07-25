3317371efc14e725719a9d5264f5ee5336510067
holl- <philipp@mholl.de>
2023-07-25 15:21:05 +0200

    [physics] Improved obstacle movement checks

--- phi/physics/fluid.py
+++ phi/physics/fluid.py
@@ -38,7 +38,17 @@ class Obstacle:
     @property
     def is_stationary(self):
         """ Test whether the obstacle is completely still. """
-        return isinstance(self.velocity, (int, float)) and self.velocity == 0 and isinstance(self.angular_velocity, (int, float)) and self.angular_velocity == 0
+        return not self.is_moving and not self.is_rotating
+
+    @property
+    def is_rotating(self):
+        available = self.angular_velocity.available if isinstance(self.angular_velocity, math.Tensor) else True
+        return not available or math.any(self.angular_velocity != 0).any
+
+    @property
+    def is_moving(self):
+        available = self.velocity.available if isinstance(self.velocity, math.Tensor) else True
+        return not available or math.any(self.velocity != 0).any
 
     def copied_with(self, **kwargs):
         warnings.warn("Obstacle.copied_with is deprecated. Use math.copy_with instead.", DeprecationWarning, stacklevel=2)
@@ -180,7 +190,7 @@ def apply_boundary_conditions(velocity: Union[Grid, PointCloud], obstacles: Unio
         if obstacle.is_stationary:
             velocity = (1 - obs_mask) * velocity
         else:
-            if obstacle.angular_velocity is not 0:  # 0 does not work in 3D
+            if obstacle.is_rotating:
                 angular_velocity = AngularVelocity(location=obstacle.geometry.center, strength=obstacle.angular_velocity, falloff=None) @ velocity
             else:
                 angular_velocity = 0
