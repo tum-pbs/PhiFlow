8262f609ab9a3c472eb7734bbfcff7ab4ef79091
holl- <philipp@mholl.de>
2023-10-12 09:59:26 +0200

    [doc] Add links to Φ-ML

--- phi/math/__init__.py
+++ phi/math/__init__.py
@@ -1,5 +1,8 @@
 """
-Alias for `phiml.math`.
+Alias for [`phiml.math`](https://tum-pbs.github.io/PhiML/phiml/math/index.html).
+
+Φ-Flow builds on the tensor functionality from [Φ-ML](https://github.com/tum-pbs/PhiML).
+Its `math` package is re-imported here for convenience.
 """
 from phiml.math import *
 
