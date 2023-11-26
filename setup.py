b753266c5bdc165a96d62f6b52f5069170ae81da
Philipp Holl <philipp.holl@tum.de>
2023-11-26 12:21:03 +0100

    [Φ] Bump version to 2.5.3, fix Φ-ML version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     url='https://github.com/tum-pbs/PhiFlow',
     include_package_data=True,
     install_requires=[
-        'phiml',
+        'phiml==1.2.1',
         'matplotlib>=3.5.0',  # also required by dash for color maps
         'packaging',
     ],
