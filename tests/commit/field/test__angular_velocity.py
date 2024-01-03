33592231871f42cdcef0b1a669e172d4a05c5d83
Philipp Holl <philipp.holl@tum.de>
2024-01-03 19:05:42 +0100

    [tests] Fix TestAngularVelocity

--- tests/commit/field/test__angular_velocity.py
+++ tests/commit/field/test__angular_velocity.py
@@ -8,6 +8,6 @@ class TestAngularVelocity(TestCase):
 
     def test_sample_at(self):
         field = AngularVelocity(location=vec(x=0, y=0))
-        self.assertEqual(channel(vector='x,y'), field.shape.channel)
         field @ CenteredGrid(0, x=4, y=3)
+        self.assertEqual(channel(vector='x,y'), (field @ CenteredGrid(0, x=4, y=3)).shape.channel)
         field @ StaggeredGrid(0, x=4, y=3)
