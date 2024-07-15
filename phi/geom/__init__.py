6cfa45a76de5f42c106490f157e8e383a7bee15b
Philipp Holl <pholl@europe.altair.com>
2024-07-15 12:39:15 +0200

    [geom] Rename sdf_from_geometry to sample_sdf

--- phi/geom/__init__.py
+++ phi/geom/__init__.py
@@ -19,7 +19,7 @@ from ._graph import Graph
 from ._mesh import Mesh, mesh, load_su2, load_gmsh, mesh_from_numpy, build_mesh
 from ._transform import embed, infinite_cylinder
 from ._heightmap import Heightmap
-from ._sdf_grid import SDFGrid, sdf_from_geometry
+from ._sdf_grid import SDFGrid, sample_sdf
 from ._sdf import SDF
 from ._geom_ops import union
 from ._convert import surface_mesh, as_sdf
