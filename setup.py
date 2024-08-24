8ed0d1b79abc42bcb83f945a5f3bc9bb76627624
Philipp Holl <philipp.holl@tum.de>
2024-08-24 17:31:01 +0200

    [Φ] Drop support for Python 3.9 and below.
    This allows usage of | in type hints

--- setup.py
+++ setup.py
@@ -51,11 +51,8 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
