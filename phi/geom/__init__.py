befc2b024db71bcc4c1548f4a5e09b082ec8e104
Philipp Holl <philipp.holl@tum.de>
2024-09-08 16:41:52 +0200

    [geom] Expose graph() constructor

--- phi/geom/__init__.py
+++ phi/geom/__init__.py
@@ -15,7 +15,7 @@ from ._geom import Geometry, GeometryException, Point, assert_same_rank, invert,
 from ._box import Box, BaseBox, Cuboid
 from ._sphere import Sphere
 from ._grid import UniformGrid
-from ._graph import Graph
+from ._graph import Graph, graph
 from ._mesh import Mesh, mesh, load_su2, load_gmsh, mesh_from_numpy, build_mesh
 from ._transform import embed, infinite_cylinder
 from ._heightmap import Heightmap
--- tests/commit/geom/test__graph.py
+++ tests/commit/geom/test__graph.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from phi import math
-from phi.geom import Graph
+from phi.geom import graph
 from phi.math import channel
 from phiml.math import wrap, instance, batch, stack, vec
 
@@ -11,13 +11,13 @@ class TestGraph(TestCase):
     def test_slice(self):
         points = wrap([(0, 0), (1.2, 0), (2, 0), (2, 1), (.8, 1), (0, 1)], instance('points'), channel(vector='x,y'))
         dense_edges = math.vec_length(math.pairwise_distances(points, 1.5))
-        dense_graph = Graph(points, dense_edges, {})
+        dense_graph = graph(points, dense_edges, {})
         dense_subgraph = dense_graph[{'points': slice(0, 2)}]
         self.assertEqual(4, math.stored_indices(dense_subgraph.edges).entries.size)
         self.assertEqual((2, 2), dense_subgraph.edges.shape.sizes)
         # --- Sparse ---
         sparse_edges = math.vec_length(math.pairwise_distances(points, 1.5, format='csr'))
-        sparse_graph = Graph(points, sparse_edges, {})
+        sparse_graph = graph(points, sparse_edges, {})
         sparse_subgraph = sparse_graph[{'points': slice(0, 2)}]
         self.assertEqual(4, math.stored_indices(sparse_subgraph.edges).entries.size)
         self.assertEqual((2, 2), sparse_subgraph.edges.shape.sizes)
@@ -26,8 +26,8 @@ class TestGraph(TestCase):
         x = vec(x=[0, 1, 2], y=0)
         deltas = math.pairwise_differences(x, max_distance=.2, format='coo')
         distances = math.vec_length(deltas)
-        graph = Graph(x, distances, {})
-        stacked = stack([graph, graph], batch('b'))
+        g = graph(x, distances, {})
+        stacked = stack([g, g], batch('b'))
         g0 = stacked.b[0]
         self.assertNotIn('b', g0.nodes.shape)
         self.assertNotIn('b', g0.edges.shape)
