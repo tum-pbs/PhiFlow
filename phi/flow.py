dad0a496aab9c6605ad99d45a74dad20f33f38bd
Philipp Holl <pholl@europe.altair.com>
2024-09-24 12:12:57 +0200

    [Φ] Add convenience imports primal,d2s

--- phi/flow.py
+++ phi/flow.py
@@ -34,10 +34,10 @@ from phiml.math.extrapolation import PERIODIC, ZERO_GRADIENT
 # Functions
 from phiml.math import (
     wrap, tensor, vec,  # Tensor creation
-    shape, spatial, channel, batch, instance, dual,
-    non_spatial, non_channel, non_batch, non_instance, non_dual,  # Shape functions (magic)
+    shape, spatial, channel, batch, instance, dual, primal,
+    non_spatial, non_channel, non_batch, non_instance, non_dual, non_primal,  # Shape functions (magic)
     unstack, stack, concat, expand, rename_dims, pack_dims, unpack_dim, flatten, cast,  # Magic Ops
-    b2i, c2b, c2d, i2b, s2b, si2d, d2i, map_s2b, map_i2b, map_c2b, map_d2c,  # dim type conversions
+    b2i, c2b, c2d, i2b, s2b, si2d, d2i, d2s, map_s2b, map_i2b, map_c2b, map_d2c,  # dim type conversions
     sign, round, ceil, floor, sqrt, exp, erf, log, log2, log10, sigmoid, soft_plus,
     sin, cos, tan, sinh, cosh, tanh, arcsin, arccos, arctan, arcsinh, arccosh, arctanh, log_gamma, factorial, incomplete_gamma,
     rotate_vector as rotate, cross_product as cross, dot, vec_normalize as normalize, length, maximum, minimum, clip,  # vector math
