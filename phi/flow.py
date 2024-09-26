7ff8c6f52ce03c7cdcec5180e49d07855687c3f9
Philipp Holl <pholl@europe.altair.com>
2024-09-26 18:56:56 +0200

    [Φ] Add tensor creators, scatter, gather to convenience imports

--- phi/flow.py
+++ phi/flow.py
@@ -33,13 +33,14 @@ from phiml.math.extrapolation import PERIODIC, ZERO_GRADIENT
 
 # Functions
 from phiml.math import (
-    wrap, tensor, vec,  # Tensor creation
+    wrap, tensor, vec, zeros, zeros_like, ones, ones_like, linspace,  # Tensor creation
     shape, spatial, channel, batch, instance, dual, primal,
     non_spatial, non_channel, non_batch, non_instance, non_dual, non_primal,  # Shape functions (magic)
     unstack, stack, concat, expand, rename_dims, pack_dims, unpack_dim, flatten, cast,  # Magic Ops
     b2i, c2b, c2d, i2b, s2b, si2d, d2i, d2s, map_s2b, map_i2b, map_c2b, map_d2c,  # dim type conversions
     sign, round, ceil, floor, sqrt, exp, erf, log, log2, log10, sigmoid, soft_plus,
     sin, cos, tan, sinh, cosh, tanh, arcsin, arccos, arctan, arcsinh, arccosh, arctanh, log_gamma, factorial, incomplete_gamma,
+    scatter, gather,
     rotate_vector as rotate, cross_product as cross, dot, vec_normalize as normalize, length, maximum, minimum, clip,  # vector math
     safe_div, length, is_finite, is_nan, is_inf,  # Basic functions
     jit_compile, jit_compile_linear, minimize, gradient as functional_gradient, gradient, solve_linear, solve_nonlinear, iterate, identity,  # jacobian, hessian, custom_gradient # Functional magic
