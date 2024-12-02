308171ece646de1735b3c7eb95252b64644dbf8e
Philipp Holl <pholl@europe.altair.com>
2024-12-02 13:09:02 +0100

    [field] Fix deprecated Hard/SoftGeometryMask

--- phi/field/_mask.py
+++ phi/field/_mask.py
@@ -3,24 +3,23 @@ from typing import Union
 
 from phi import math
 from phi.geom import Geometry
-from ._field import Field
-from phiml.math import Tensor
+from ._field import FieldInitializer
+from phiml.math import Tensor, Extrapolation
 
 
-class HardGeometryMask(Field):
+class HardGeometryMask(FieldInitializer):
     """
-    Deprecated since version 1.3. Use `phi.field.mask()` or `phi.field.resample()` instead.
+    Deprecated since version 2.3. Use `phi.field.mask()` or `phi.field.resample()` instead.
     """
-
     def __init__(self, geometry: Geometry):
-        super().__init__(geometry, 1, 0)
+        self.geometry = geometry
         warnings.warn("HardGeometryMask and SoftGeometryMask are deprecated. Use field.mask or field.resample instead.", DeprecationWarning, stacklevel=2)
 
     @property
     def shape(self):
         return self.geometry.shape.non_channel
 
-    def _sample(self, geometry: Geometry, **kwargs) -> Tensor:
+    def _sample(self, geometry: Geometry, at: str, boundaries: Extrapolation, **kwargs) -> math.Tensor:
         return math.to_float(self.geometry.lies_inside(geometry.center))
 
     def __getitem__(self, item: dict):
@@ -29,14 +28,14 @@ class HardGeometryMask(Field):
 
 class SoftGeometryMask(HardGeometryMask):
     """
-    Deprecated since version 1.3. Use `phi.field.mask()` or `phi.field.resample()` instead.
+    Deprecated since version 2.3. Use `phi.field.mask()` or `phi.field.resample()` instead.
     """
     def __init__(self, geometry: Geometry, balance: Union[Tensor, float] = 0.5):
         warnings.warn("HardGeometryMask and SoftGeometryMask are deprecated. Use field.mask or field.resample instead.", DeprecationWarning, stacklevel=2)
         super().__init__(geometry)
         self.balance = balance
 
-    def _sample(self, geometry: Geometry, **kwargs) -> Tensor:
+    def _sample(self, geometry: Geometry, at: str, boundaries: Extrapolation, **kwargs) -> math.Tensor:
         return self.geometry.approximate_fraction_inside(geometry, self.balance)
 
     def __getitem__(self, item: dict):
