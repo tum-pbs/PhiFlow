0064e91e32f8f70445857682cdf57b35e7894057
Philipp Holl <philipp.holl@tum.de>
2026-05-10 12:48:28 +0200

    [tests] Remove TensorFlow from tests

--- tests/commit/test_ci_installation.py
+++ tests/commit/test_ci_installation.py
@@ -5,12 +5,11 @@ import phi
 
 class TestCIInstallation(TestCase):
 
-    def test_detect_tf_torch_jax(self):
+    def test_detect_torch_jax(self):
         backends = phi.detect_backends()
         names = [b.name for b in backends]
         self.assertIn('torch', names)
         self.assertIn('jax', names)
-        self.assertIn('tensorflow', names)
 
     def test_verify(self):
         phi.verify()
